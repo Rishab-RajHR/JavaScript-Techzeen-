6a5d956b367373603f9d0e595abe83e0f81efa9f
Rishab Raj <rr0948555@gmail.com>
2026-02-12 22:58:01 +0530

    Operators in JavaScript

---
+++ js/operators.js
@@ -0,0 +1,21 @@
+/*
+operand: data on which operations are performed
+operator: type of operation
+expression: combination of operator and operands
+e.g:
+b+c;
+*/
+
+let x = 2;
+let y = 4;
+
+let z;
+
+z = x+y;
+q = x-y;
+p = x*y;
+r = x/y;
+console.log(z);
+console.log(q);
+console.log(p);
+console.log(r);
---
+++ js/variable.js
@@ -0,0 +1,17 @@
+// console.log("JavaScript")
+// document.getElementById("heading").innerHTML = "Hi Buddy!!!";
+// document.write("Hii");
+
+// Declare
+// var firstName;
+// // Initialize
+// firstName = 5;
+// console.log(firstName);
+
+// Declare + Initialize
+var firstName = 5;
+console.log(firstName);
+
+const rndNum = 3;
+// rndNum = 4;
+console.log(rndNum);
