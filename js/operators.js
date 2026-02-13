8b3f1fd6140a2f11e481eedf2efe9d9500bd8028
Rishab Raj <rr0948555@gmail.com>
2026-02-13 23:02:09 +0530

    Data Type

--- js/operators.js
+++ js/operators.js
@@ -11,11 +11,52 @@ let y = 4;
 
 let z;
 
-z = x+y;
-q = x-y;
-p = x*y;
-r = x/y;
-console.log(z);
-console.log(q);
-console.log(p);
-console.log(r);
+// z = x+y;
+// q = x-y;
+// p = x*y;
+// r = x/y;
+// t = y%x; // (modulus operator-> remainder)
+// console.log(z);
+// console.log(q);
+// console.log(p);
+// console.log(r);
+// console.log(t);
+
+// Shorthand Property of above line
+// x = x + y;
+// x += y;
+// console.log(x);
+
+
+// For Power we use operator 
+// z = x ** 3;
+// console.log(z);
+
+
+
+
+// Increment and Decrement
+
+// x++; // 1 increment
+// x--; // 1 decrement
+// console.log(x);
+
+
+
+
+// a = 2; // Integer
+// b = "2"; // String
+
+// // concatenation-> combine
+// console.log(a+b); // output: 22
+
+
+// BODMAS -> Bracket Of Division Multiplication Addition Subtraction
+// z = x+2/2;
+// console.log(z);
+
+
+
+// ***************Data Type*********************
+// let x = 2;  // int
+// let y = "hello"; // string
