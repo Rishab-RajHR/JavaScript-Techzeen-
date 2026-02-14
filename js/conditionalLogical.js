5025641776da75178d9eaae70f7f062c1bff2a03
Rishab Raj <rr0948555@gmail.com>
2026-02-14 23:04:29 +0530

    Comparison

---
+++ js/conditionalLogical.js
@@ -0,0 +1,14 @@
+/*
+Comparison Operator:
+Compare
+
+1) == -> equal to (numbers are equal in value)
+e.g:
+  3 == 4; -> false
+  2 == "2" -> true (because it only comapres value not data type)
+
+2) === -> strict equal to (numbers are equal in value and data type)
+e.g:
+  3 === 4; false
+  2 === "2"; false (because data type of 2 is number and data type of "2" is string)
+ */
