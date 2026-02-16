e79d2aedf5c72097ca854badf6116a6719f311ec
Rishab Raj <rr0948555@gmail.com>
2026-02-16 22:59:39 +0530

    If Else

---
+++ js/ifElse.js
@@ -0,0 +1,40 @@
+/*
+Syntax of if and else:
+if(condition){
+
+}
+else {
+  
+}
+
+
+if Else if Ladder:
+
+if(condition){
+
+}
+else if(condition){
+  
+}
+else{
+  
+}
+
+
+
+Ternary Operator:
+variable (condiiton) ? (output if the value is true) : (output if the value is false)
+
+Nullish Coalescing Operator:
+syntax:
+valueToCheck ?? defaultValue
+*/ 
+
+let number = 4;
+
+// if(number>2){
+//    document.write("Number is greater than 2");
+// }
+// else {
+//  document.write("Number is less than 2");
+// }
