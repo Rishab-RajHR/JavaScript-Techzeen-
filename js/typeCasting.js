d194c2ec7e2e0ba1100c4e86f58ed53e61387451
Rishab Raj <rr0948555@gmail.com>
2026-02-22 22:47:00 +0530

    typecasting

---
+++ js/typeCasting.js
@@ -0,0 +1,93 @@
+// Converting data type from one to another . That is from string to boolean
+
+// let number = 4;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+
+// let number = 4 + null;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+
+// let number = "3" + 2;
+// document.writeln(number);
+// document.writeln(typeof number); // string
+
+
+// When subtraction then convert string to number
+// let number = "3" - 2;
+// document.writeln(number);
+// document.writeln(typeof number); //number
+
+
+// let number = "3" * 2;
+// document.writeln(number);
+// document.writeln(typeof number); // NaN
+
+
+/*
+
+TypeCasting
+1) String()
+2) Number()
+3) Boolean()
+
+*/
+
+// let oldType = 2;
+// document.writeln(typeof oldType);  // number
+
+// let newType = String(oldType);
+// document.writeln(typeof newType);  // string
+
+
+// let oldType = "32";
+// document.writeln(typeof oldType);  // string
+
+// let newType = Number(oldType);
+// document.writeln(typeof newType);  // number
+
+
+
+// let oldType = "32";
+// document.writeln(typeof oldType);  // string
+
+// let newType = Boolean(oldType);
+// document.writeln(typeof newType);  // boolean
+
+// let oldType = true;
+// document.writeln(typeof oldType);  // boolean
+
+// let newType = Number(oldType);
+// document.writeln(newType);
+// document.writeln(typeof newType);   // number
+
+
+
+// let oldType = false;
+// document.writeln(typeof oldType);  // boolean
+
+// let newType = String(oldType);
+// document.writeln(typeof newType);   // string
+// document.writeln(newType);   // string
+
+
+
+// let oldType = 32;
+// document.writeln(typeof oldType);  // number
+
+// let newType = Boolean(oldType);
+// document.writeln(newType); 
+// document.writeln(typeof newType);   // boolean
+
+
+
+let oldType = "";
+document.writeln(typeof oldType); // Empty
+
+let newType = Number(oldType);
+document.writeln(newType); 
+document.writeln(typeof newType);    // number
+
+
