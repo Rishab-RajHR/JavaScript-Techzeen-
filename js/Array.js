fedb81521ea69d9f083be91b26c902bea0d0f60d
Rishab Raj <rr0948555@gmail.com>
2026-02-24 22:29:32 +0530

    Array Operations

---
+++ js/Array.js
@@ -0,0 +1,35 @@
+//************** Array Operations **********
+
+let teams = ["India", "Australia", "China", "South Africa"];
+
+// console.log(teams);
+// console.log(teams.sort());
+// console.log(teams.length);
+
+// push() => Element will be added at last
+// teams.push("England");
+// console.log(teams);
+
+// unshift() => Element will be added at starting
+// teams.unshift("England");
+// console.log(teams);
+
+
+// pop() => Element will be removed from last
+// teams.pop();
+// console.log(teams);
+
+
+// shift() => Element will be removed from starting
+// teams.shift()
+// console.log(teams);
+
+// splice() => remove the element from middle
+// teams.splice(2, 1);
+// teams.splice(2, 2);
+// console.log(teams);
+
+// To Empty the Array
+teams = [];
+teams.length = 0;
+console.log(teams);
