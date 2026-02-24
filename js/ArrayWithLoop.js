f933f5ffdb18f9bd3448db11cd1f9d3ee6e961f7
Rishab Raj <rr0948555@gmail.com>
2026-02-24 22:13:54 +0530

    Array

---
+++ js/ArrayWithLoop.js
@@ -0,0 +1,25 @@
+// Array => Storing multiple elemets in a single variable
+
+// let students = "Alex";
+// let students1 = "Tovino";
+// let students2 = "George";
+
+// let students = ["Alex", "Bob", "George", "Basil", "Tovino"]
+
+// let students = new Array("Alex", "Bob", "George", "Basil", "Tovino");
+
+// console.log(students[2]); // Accesing Index
+
+
+// let info = [1, "Ali", 60.60];
+// console.log(info);
+// console.log(info[2]);
+
+
+
+let subjects = ["Maths", "Science", "English", "Computer"];
+for(let i=0; i<4; i++) 
+{
+    console.log(subjects[i]);
+    console.log("\n");
+}
