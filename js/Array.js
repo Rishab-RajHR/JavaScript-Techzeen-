3b0cdbe24880646f6d6fb3adb134196580d61148
Rishab Raj <rr0948555@gmail.com>
2026-02-25 22:17:27 +0530

    Concatenate

--- js/Array.js
+++ js/Array.js
@@ -30,6 +30,33 @@ let teams = ["India", "Australia", "China", "South Africa"];
 // console.log(teams);
 
 // To Empty the Array
-teams = [];
-teams.length = 0;
-console.log(teams);
+// teams = [];
+// teams.length = 0;
+// console.log(teams);
+
+
+// let index = teams.indexOf("India");
+// console.log(teams);
+// console.log(index);
+
+
+// Accessing the value that would be true or false if this is the Array
+// let theam = "Australia";
+// console.log(Array.isArray(theam));  // False
+
+
+
+// Split the array element
+// let message = "Hi I'm Alex Pandian and This is My Youtube Channel";
+
+// let splitting = message.split(" ");
+// console.log(splitting);
+
+
+
+// Concatenation => joining the array
+let arr1 = ["Alex", "Aman", "Bob"];
+let arr2 = ["Ayesha", "Naveen", "Axar"];
+
+let newArray = arr1.concat(arr2);
+console.log(newArray);
