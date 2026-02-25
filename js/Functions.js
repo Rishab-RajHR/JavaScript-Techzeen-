5c27b9c3b84af3b565c73c151be7a7d1c249d034
Rishab Raj <rr0948555@gmail.com>
2026-02-25 22:40:53 +0530

    Functions

---
+++ js/Functions.js
@@ -0,0 +1,29 @@
+// Function Define
+function greet()
+{
+    console.log("Hello I'm Alex Pandian");
+}
+greet();
+greet();
+
+
+// Add Two Numbers
+function add(a , b){
+    console.log(a+b);
+}
+// Calling functions
+add(5,6);
+add(5,6);
+add(5,6);
+
+
+
+// Initial name at time of defining
+
+function greete(name)
+{
+   console.log(`Hello ${name}!`);
+}
+let name = prompt("Enter Your Name: ");
+
+greete(name);
