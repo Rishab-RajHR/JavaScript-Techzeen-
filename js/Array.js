8667906b127266e06fca61e2b585d40398aabb68
Rishab Raj <rr0948555@gmail.com>
2026-02-25 22:28:31 +0530

    For Loop

--- js/Array.js
+++ js/Array.js
@@ -55,8 +55,39 @@ let teams = ["India", "Australia", "China", "South Africa"];
 
 
 // Concatenation => joining the array
-let arr1 = ["Alex", "Aman", "Bob"];
-let arr2 = ["Ayesha", "Naveen", "Axar"];
+// let arr1 = ["Alex", "Aman", "Bob"];
+// let arr2 = ["Ayesha", "Naveen", "Axar"];
 
-let newArray = arr1.concat(arr2);
-console.log(newArray);
+// let newArray = arr1.concat(arr2);
+// let newArray = arr2.concat(arr1);
+// console.log(newArray);
+
+
+
+//     0["Maths", "Algebra"],
+//     1["Bio", "Zoology"],
+//     2["Java", "JavaScript"
+
+
+// Multidimensional Array => Array inside array
+
+// let books = [
+//     ["Maths", "Algebra"],
+//     ["Bio", "Zoology"],
+//     ["Java", "JavaScript"]
+// ];
+
+// let arr = books[1][0]; => Bio
+// let arr = books[2][1];  => JavaScript
+
+// console.log(arr);
+
+
+
+
+let teamse = ["India", "Australia", "Nepal", "England"];
+
+for(let index = 0; index < teamse.length; index++)
+{
+    console.log(`Elements of Teams Array are: ${index} of ${teamse[index]}`);
+}
