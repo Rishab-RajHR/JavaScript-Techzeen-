1242130cbc4921ec9ba49276ba0eb57f87f4a059
Rishab Raj <rr0948555@gmail.com>
2026-03-04 08:33:56 +0530

    Math Object

--- js/MathObject.js
+++ js/MathObject.js
@@ -5,4 +5,12 @@ let result;
 // result = Math.abs(-22); // Negative to positive
 //result = Math.round(2.6); //round up
 // result = Math.round(2.2); //round down
+// result = Math.PI; // for pi value
+// result = Math.ceil(7.2); // round up
+// result = Math.floor(7.8); // round down
+// result = Math.trunc(7.8); // truncate the decimal value
+// result = Math.pow(2,3); //first argument -> base, second argument -> power
+// result = Math.sqrt(64); //square root
+// result = Math.min(1,100,200,-3,0.5); // find the minimum value
+// result = Math.max(1,100,200,-3,0.5); // find the maximum value
 console.log(result);
