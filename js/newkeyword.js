d0853edf39f7cdd8eb28d75baf897d86a6ec9e6e
Rishab Raj <rr0948555@gmail.com>
2026-03-06 07:56:51 +0530

    New Keyword

---
+++ js/newkeyword.js
@@ -0,0 +1,8 @@
+// New Keyword => used to create instance of an object that has a constructor function.
+
+let employee = {
+   name: "Alex",
+   salary: 5000
+}
+console.log(employee.name)
+console.log(employee.salary)
