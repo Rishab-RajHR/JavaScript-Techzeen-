71f276f258aea9c5107f953bbbb569ea1bf71d0d
Rishab Raj <rr0948555@gmail.com>
2026-03-06 08:00:30 +0530

    New Keyword

--- js/newkeyword.js
+++ js/newkeyword.js
@@ -1,8 +1,21 @@
 // New Keyword => used to create instance of an object that has a constructor function.
 
-let employee = {
-   name: "Alex",
-   salary: 5000
-}
-console.log(employee.name)
-console.log(employee.salary)
+// let employee = {
+//    name: "Alex",
+//    salary: 5000
+// }
+// console.log(employee.name)
+// console.log(employee.salary)
+
+
+// object creation using new keyword
+
+let employee = new Object();
+
+employee.name = "Alex Pandian";
+employee.salary = 60000;
+
+console.log(employee); // for the whole object
+
+console.log(employee.name);
+console.log(employee.salary);
