671fd50885f893e8e22d185f2711d0119b776382
Rishab Raj <rr0948555@gmail.com>
2026-03-08 14:00:03 +0530

    Getter and Stter

---
+++ js/GetterSetter.js
@@ -0,0 +1,14 @@
+/*
+*)getters ---> can be used to access the properties of an object
+Syntax: 
+get methodName()
+{
+   // code for getting the property
+}
+*)setters ---> can be used to execute a function whenever a specified property is attempted to be changed
+Syntax:
+set methodName(paramter)
+{
+   // code for setting the property
+}
+*/
