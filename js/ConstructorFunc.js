e49ea54c448527ed7631ef8e213394f6c2e036f3
Rishab Raj <rr0948555@gmail.com>
2026-03-09 12:54:09 +0530

    Contructot

--- js/ConstructorFunc.js
+++ js/ConstructorFunc.js
@@ -1,6 +1,6 @@
 /*
 
- Object Constructor Function
+ ******* Object Constructor Function *******
 
  creates and initializes an object instance.
  a constructor gets called when an object is created using the new keyword.
@@ -11,3 +11,16 @@
   }
 
 */
+
+/* let employee1 = {
+    firstName : "Alex",
+    lastName : "Pandian",
+    salary : 50000,
+    department : "IT"
+}
+let employee2 = {
+    firstName : "Tovino",
+    lastName : "Pandian",
+    salary : 60000,
+    department : "AI"
+} */
