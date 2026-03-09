2c8f8a338f29292855f57273ffa6056681b5b138
Rishab Raj <rr0948555@gmail.com>
2026-03-09 12:45:04 +0530

    Constructor

---
+++ js/ConstructorFunc.js
@@ -0,0 +1,13 @@
+/*
+
+ Object Constructor Function
+
+ creates and initializes an object instance.
+ a constructor gets called when an object is created using the new keyword.
+
+ Syntax: 
+ function MethodName(paramter list){
+     code.....    
+  }
+
+*/
