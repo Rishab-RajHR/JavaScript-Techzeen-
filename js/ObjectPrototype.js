0b0b3440b7da08c53778d1b23ed563155fffdead
Rishab Raj <rr0948555@gmail.com>
2026-03-10 13:10:26 +0530

    Object Prototype

---
+++ js/ObjectPrototype.js
@@ -0,0 +1,16 @@
+/*
+ *************** Object Prototype ******************
+
+ Prototypes are the mechanism by which JavaScript objects inherits features from one another
+
+ if we want to add new properties and methods at a later point of time, and these properties need sharing across all the instances(objects), then the prototype will be very handy.
+*/
+
+let teacher = {
+    id : 1,
+    name : "Alex"
+}
+let student = {
+    age : 18
+}
+// console.log(teacher.age); --> not working
