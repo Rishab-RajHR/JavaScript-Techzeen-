2a3002d22db5ef57e5d799ac299a1a436c7dde42
Rishab Raj <rr0948555@gmail.com>
2026-03-11 13:32:24 +0530

    Hoisting in JavaScript

---
+++ js/Hoisting.js
@@ -0,0 +1,9 @@
+/*
+Hoisting In JavaScript => The Default behavior of moving all the decalarations at the top of the scope before code execution.
+*/
+
+greetings();
+
+function greetings(){
+     console.log("Good Night");
+}
