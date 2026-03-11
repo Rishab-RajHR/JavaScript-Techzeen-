ddcf11ea9f5d4e3da535f5fb3e22a88c85f7c0c9
Rishab Raj <rr0948555@gmail.com>
2026-03-11 13:50:58 +0530

    Hoisting

--- js/Hoisting.js
+++ js/Hoisting.js
@@ -11,8 +11,54 @@ Hoisting In JavaScript => The Default behavior of moving all the declarations at
 
 // Variable --- Var
 
-var a;  // decalaration
+// var a;  // decalaration
+// a = undefined
 
-a = 10;  // assignment / initialization
+// a = 10;  // assignment / initialization
 
-console.log(a);
+// console.log(a);
+
+// var a;  // decalaration
+
+
+// a = 7;
+// console.log(a);
+
+// var a;
+
+// Declaration is hoisted assignment not
+
+
+// Variable -- let (blocked scoped)
+  // let b;
+  // b = 2;
+  // console.log(b);
+
+
+
+// function expression --> Hoisting is not working
+// var greet = function(){
+//     console.log("first");
+// }
+// greet();
+
+
+
+
+// function  show() {
+    
+//     age = 19;
+//     console.log(age);
+//     var age;
+// }
+// show();
+
+
+// For giving the error don't use var because it is function scoped
+
+// 'use strict';
+// age = 18;
+// console.log(age);
+
+
+
