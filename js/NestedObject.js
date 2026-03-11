ea002688bb19c8e53a42b50401dbada4d08bb5fe
Rishab Raj <rr0948555@gmail.com>
2026-03-11 13:23:11 +0530

    Nested Object

--- js/NestedObject.js
+++ js/NestedObject.js
@@ -14,8 +14,15 @@ let student = {
              email : "alex112@gmail.com",
              phone : '123-456-789'
          }
+    },
+    addressInfo : {
+        city : 'Noida'
     }
+
 }
+
 // console.log(student)
 // console.log(student.personalInfo);
-console.log(student.personalInfo.firstName);
+// console.log(student.personalInfo.firstName);
+// console.log(student.personalInfo.contactInfo.email);
+console.log(student.addressInfo.city);
