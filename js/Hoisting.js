81b8bc2c924b00e454bae8b67653d2938219652f
Rishab Raj <rr0948555@gmail.com>
2026-03-11 13:35:38 +0530

    Variable Hoisting

--- js/Hoisting.js
+++ js/Hoisting.js
@@ -1,9 +1,18 @@
 /*
-Hoisting In JavaScript => The Default behavior of moving all the decalarations at the top of the scope before code execution.
+Hoisting In JavaScript => The Default behavior of moving all the declarations at the top of the scope before code execution.
 */
 
-greetings();
+// greetings();
 
-function greetings(){
-     console.log("Good Night");
-}
+// function greetings(){
+//      console.log("Good Night");
+// }
+
+
+// Variable --- Var
+
+var a;  // decalaration
+
+a = 10;  // assignment / initialization
+
+console.log(a);
