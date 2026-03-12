736f98612a1d94a8fde9999afca6f29fa8c01c4d
Rishab Raj <rr0948555@gmail.com>
2026-03-12 13:26:04 +0530

    InnerHTML

--- js/DOM.js
+++ js/DOM.js
@@ -1,2 +1,5 @@
 let element = document.getElementById("me");
-        console.log(element);
+      // console.log(element);
+      // console.log(element.innerHTML);
+      element.innerHTML = "I am from Delhi";
+      console.log(element.innerHTML);
