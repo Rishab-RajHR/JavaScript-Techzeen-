3efbf990ffc47d0fd4c6507a8fe73b8c92a7f73f
Rishab Raj <rr0948555@gmail.com>
2026-03-13 13:48:07 +0530

    Class

--- js/DOM2.js
+++ js/DOM2.js
@@ -1,2 +1,6 @@
 let element = document.getElementsByClassName("programming");
-console.log(element);
+// console.log(element.length);
+// element.innerHTML = "Hi";---->not working
+for(let i=0; i<element.length; i++){
+    element[i].innerHTML = "Jalwa Hai Humara";
+}
