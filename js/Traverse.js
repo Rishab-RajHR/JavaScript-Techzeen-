f373513cd641f7fb8d05113fd4fe884f4d699815
Rishab Raj <rr0948555@gmail.com>
2026-03-15 13:28:53 +0530

    Traversing

--- js/Traverse.js
+++ js/Traverse.js
@@ -1,5 +1,15 @@
 // For Parent Elements
-let element = document.getElementById('me');
+// let element = document.getElementById('me').parentElement;
+// console.log(element);
+// element.innerHTML = "Alex";  // remove all li and replace it with Alex
+// let parentElement = element.parentElement;
+// console.log(parentElement);
+
+
+
+// For First and Last Child
+let element = document.getElementById('hi')
 // console.log(element)
-let parentElement = element.parentElement;
-console.log(parentElement);
+// let childElement = element.firstElementChild;
+let childElement = element.lastElementChild;
+console.log(childElement)
