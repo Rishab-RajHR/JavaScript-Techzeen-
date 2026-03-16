767db2ef8eee00d4c8264ebb11ab4a5bafcce0e3
Rishab Raj <rr0948555@gmail.com>
2026-03-16 12:29:36 +0530

    InnerHTML

--- js/innerHTML.js
+++ js/innerHTML.js
@@ -1,4 +1,5 @@
 let element = document.getElementById('me');
 // console.log(element);
-let result = element.innerHTML;
-console.log(result);
+// let result = element.innerHTML;
+// console.log(result);
+element.innerHTML = "<h1>Tovino Thomas</h1>"
