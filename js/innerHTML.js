ab5537618b91522ff3e26edaa94a9df64a12b0da
Rishab Raj <rr0948555@gmail.com>
2026-03-16 12:33:55 +0530

    innerHTML

--- js/innerHTML.js
+++ js/innerHTML.js
@@ -1,5 +1,16 @@
-let element = document.getElementById('me');
+// let element = document.getElementById('me');
 // console.log(element);
 // let result = element.innerHTML;
 // console.log(result);
-element.innerHTML = "<h1>Tovino Thomas</h1>"
+// element.innerHTML = "<h1>Tovino Thomas</h1>"
+
+let element = document.getElementsByClassName('you');
+// console.log(element);
+// let result = element.innerHTML; --> not working
+// console.log(result);
+
+// For accessing the array we use loops
+for(let i=0; i<element.length; i++){
+      let result = element[i].innerHTML;
+      console.log(result);
+}
