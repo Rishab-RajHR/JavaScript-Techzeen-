c7ad0d0043ca658e5f4936345208dc007401922d
Rishab Raj <rr0948555@gmail.com>
2026-03-17 13:42:40 +0530

    AppendChild

--- js/CreateAppend.js
+++ js/CreateAppend.js
@@ -1,17 +1,30 @@
-let element = document.getElementById('me');
-let heading = document.createElement("h1");
+// let element = document.getElementById('me');
+// let heading = document.createElement("h1");
 
 // how to add class and id
-heading.className = "js course";
-heading.id = "tillu_id";
+// heading.className = "js course";
+// heading.id = "tillu_id";
 
 // Append Text uinsg method
 // let text = document.createTextNode("This is Create Append");
 // heading.appendChild(text);
 
 // Append Text using property
-heading.textContent = "This is Robo Boys";
+// heading.textContent = "This is Robo Boys";
 // element.appendChild(heading);
 
 // Append on body tag
-document.body.appendChild(heading);
+// document.body.appendChild(heading);
+
+
+// Reading a text from an element
+// let para = document.getElementById("para");
+// console.log(para.textContent);
+
+
+let item = document.getElementById('items');
+let elementItem = document.createElement('li');
+elementItem.textContent = "Item From DOM Manipulation";
+item.appendChild(elementItem);
+
+
