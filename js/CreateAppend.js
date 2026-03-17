7df13e4d95c04744758deb81a382573521ce727a
Rishab Raj <rr0948555@gmail.com>
2026-03-17 13:33:41 +0530

    Append

--- js/CreateAppend.js
+++ js/CreateAppend.js
@@ -1,6 +1,17 @@
 let element = document.getElementById('me');
 let heading = document.createElement("h1");
 
-let text = document.createTextNode("This is Create Append");
-heading.appendChild(text);
-element.appendChild(heading);
+// how to add class and id
+heading.className = "js course";
+heading.id = "tillu_id";
+
+// Append Text uinsg method
+// let text = document.createTextNode("This is Create Append");
+// heading.appendChild(text);
+
+// Append Text using property
+heading.textContent = "This is Robo Boys";
+// element.appendChild(heading);
+
+// Append on body tag
+document.body.appendChild(heading);
