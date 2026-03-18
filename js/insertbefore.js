f8b4e77bdab0cf8ef383d4b61a02e69c25735a08
Rishab Raj <rr0948555@gmail.com>
2026-03-18 13:53:23 +0530

    InsertBefore

--- js/insertbefore.js
+++ js/insertbefore.js
@@ -1,8 +1,21 @@
-let list = document.getElementById("items");
+// let list = document.getElementById("items");
 
-let item = document.createElement("li");
-item.textContent = "Item from DOM";
+// let item = document.createElement("li");
+// item.textContent = "Item from DOM";
 
-let position = list.firstElementChild;
+// let position = list.firstElementChild;
+// let position = list.firstElementChild.nextElementSibling;
 
-list.insertBefore(item, position)
+// list.insertBefore(item, position)
+
+
+let element = document.body;   // parent selected
+
+let newElement = document.createElement("h1");
+newElement.textContent = "RishabZeen";
+
+// element.appendChild(newElement);
+
+let list = document.getElementById('items');
+
+element.insertBefore(newElement, list);
