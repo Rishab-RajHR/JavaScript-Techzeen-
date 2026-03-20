cc781fdedb4408ede979ca1211436864080419c7
Rishab Raj <rr0948555@gmail.com>
2026-03-20 13:25:36 +0530

    Replace

--- js/replaceChild.js
+++ js/replaceChild.js
@@ -3,6 +3,6 @@ let parent = document.getElementById("items");
 let newItem = document.createElement("li");
 newItem.textContent = "Replace Element";
 
-let replace = parent.firstElementChild;
+let replace = parent.firstElementChild.nextElementSibling;
 
 parent.replaceChild(newItem, replace);
