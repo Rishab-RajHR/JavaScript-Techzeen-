1ad4ae2e7302d52ea1a8d043d3488bbeb7664b75
Rishab Raj <rr0948555@gmail.com>
2026-03-20 13:43:26 +0530

    Insert Adjacent

---
+++ js/insertAdjacent.js
@@ -0,0 +1,11 @@
+let selectedElement  = document.getElementById("main");
+
+let newElement = "<h1>Alex Pandian</h1>";
+
+// selectedElement.insertAdjacentHTML("beforebegin", newElement);
+
+// selectedElement.insertAdjacentHTML("afterbegin", newElement);
+
+// selectedElement.insertAdjacentHTML("beforeend", newElement);
+
+selectedElement.insertAdjacentHTML("afterend", newElement);
