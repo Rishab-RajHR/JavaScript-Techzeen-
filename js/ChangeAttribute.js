4aa0f2bc718ce0e272b0f53d39e99aee3eaada32
Rishab Raj <rr0948555@gmail.com>
2026-03-21 08:22:17 +0530

    Change Attribute

--- js/ChangeAttribute.js
+++ js/ChangeAttribute.js
@@ -4,5 +4,12 @@ let heading = document.getElementById("main");
 heading.setAttribute("class","heading-class");
 
 // find the value of an attribute
-let value = heading.getAttribute("class");
-console.log(value);
+// let value = heading.getAttribute("class");
+// console.log(value);
+
+// For removing an attribute
+// heading.removeAttribute("class");
+
+// Check attribute 
+let result = heading.hasAttribute("name");
+console.log(result);
