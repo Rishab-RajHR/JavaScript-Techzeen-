cce3ab64eb670e069e7369402ec50e37a3efa3e1
Rishab Raj <rr0948555@gmail.com>
2026-03-22 13:37:12 +0530

    Hello

--- js/InlineStyle.js
+++ js/InlineStyle.js
@@ -1,1 +1,8 @@
 let selectedElement = document.getElementById("main");
+
+// First Method
+// selectedElement.style.cssText = "background-color: aqua; color: white;";
+
+
+// Second Method
+selectedElement.setAttribute("style","color: white; background-color: purple;")
