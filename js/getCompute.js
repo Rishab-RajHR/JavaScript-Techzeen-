0e1c6665d8d956689a0d631ca7e8104d6f1aabd9
Rishab Raj <rr0948555@gmail.com>
2026-03-23 13:13:38 +0530

    Get Compute Style

---
+++ js/getCompute.js
@@ -0,0 +1,8 @@
+let selectedElement = document.getElementById("main");
+
+let styling = getComputedStyle(selectedElement);
+
+// console.log(styling);
+// console.log(styling.color);
+
+console.log(styling.backgroundColor);
