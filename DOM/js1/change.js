eb006f6a52e7a19042880e4daf26f045ea4c933e
Rishab Raj <rr0948555@gmail.com>
2026-03-24 13:29:46 +0530

    Change

--- DOM/js1/change.js
+++ DOM/js1/change.js
@@ -3,9 +3,42 @@ let element = document.getElementById("box");
 // find className of an element
 // console.log(element.className);
 
+
 // how to add class using className Property
-element.className += " color";
+// element.className += " color";
 
 
 // How to chack className List of an element 
-console.log(element.classList);
+// console.log(element.classList);
+
+
+// Accessing individual element using for-of  loop
+
+// for(let style of element.classList){
+//      console.log(style)
+// }
+
+
+
+// how to add a className
+// element.classList.add("color");
+
+
+// how to remove a className
+// element.classList.remove("square");
+
+
+// how to replace a className
+// element.classList.replace("square","color");
+
+
+// how to check existence of a class
+// let result = element.classList.contains("color");
+// console.log(result);
+
+
+// toggle class add/remove
+// element.classList.toggle("square");
+element.classList.toggle("color");
+
+
