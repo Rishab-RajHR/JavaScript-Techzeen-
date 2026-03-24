7a82fcf9dd2dae721d668a67dd5180ea94d6f319
Rishab Raj <rr0948555@gmail.com>
2026-03-24 13:16:00 +0530

    CSS

--- DOM/js1/change.js
+++ DOM/js1/change.js
@@ -1,4 +1,11 @@
 let element = document.getElementById("box");
 
 // find className of an element
-console.log(element.className)
+// console.log(element.className);
+
+// how to add class using className Property
+element.className += " color";
+
+
+// How to chack className List of an element 
+console.log(element.classList);
