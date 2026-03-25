cc389bcd082dac0439134c0096a5aece5a257db2
Rishab Raj <rr0948555@gmail.com>
2026-03-25 12:51:14 +0530

    DOM Events

--- DOM/js1/DomEvents.js
+++ DOM/js1/DomEvents.js
@@ -10,3 +10,22 @@ let btn = document.getElementById("btn");
 
 // first paramter -> which event, second parameter -> which function
 btn.addEventListener('click',btnClick);
+
+
+// by using anoonymous function
+btn.addEventListener('click',function(){
+      btn.style.backgroundColor = "aqua";
+});
+
+
+// There can be more than one event listener
+
+// Mouse Over Event
+btn.addEventListener('mouseover', function(){
+     console.log("Mouse Over Event!")
+});
+
+// Mouse Out Event
+btn.addEventListener('mouseout', function(){
+     console.log("Mouse Out Event!")
+});
