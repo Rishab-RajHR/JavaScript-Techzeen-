f5a63dea914bb45c8e56251cdfffd7e9598fd92f
Rishab Raj <rr0948555@gmail.com>
2026-03-25 12:43:22 +0530

    Event

--- DOM/js1/DomEvents.js
+++ DOM/js1/DomEvents.js
@@ -1,3 +1,12 @@
 /*
 DOM EVENTS => Are actions or occurence that happens in a web page, like clicking a button, typing on a keyboard, or moving the mouse, which can be detected and responded to by JavaScript code.
 */
+
+function btnClick(){
+   console.log("Button Was Clicked!")
+}
+
+let btn = document.getElementById("btn");
+
+// first paramter -> which event, second parameter -> which function
+btn.addEventListener('click',btnClick);
