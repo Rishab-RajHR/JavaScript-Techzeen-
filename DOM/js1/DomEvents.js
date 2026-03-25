2803f16623bddc52eb23ffe84879169efce1ed45
Rishab Raj <rr0948555@gmail.com>
2026-03-25 12:35:30 +0530

    DOM Events

---
+++ DOM/js1/DomEvents.js
@@ -0,0 +1,3 @@
+/*
+DOM EVENTS => Are actions or occurence that happens in a web page, like clicking a button, typing on a keyboard, or moving the mouse, which can be detected and responded to by JavaScript code.
+*/
