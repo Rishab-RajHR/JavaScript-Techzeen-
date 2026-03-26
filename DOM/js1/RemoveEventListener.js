7e12e6a5e517ac827a0e9c606e4072a199127bdd
Rishab Raj <rr0948555@gmail.com>
2026-03-26 13:05:32 +0530

    Remove

---
+++ DOM/js1/RemoveEventListener.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+
+function firstClick() {
+     console.log("1st Time Click");
+}
+
+function secondClick() {
+     console.log("2nd Time Click");
+}
+
+btn.addEventListener('click', firstClick);
+btn.addEventListener('click', secondClick);
