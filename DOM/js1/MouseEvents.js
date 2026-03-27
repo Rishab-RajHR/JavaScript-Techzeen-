deb0b27d726107750e7c7ee2cac5129c68b50110
Rishab Raj <rr0948555@gmail.com>
2026-03-27 13:14:18 +0530

    Mouse Events

---
+++ DOM/js1/MouseEvents.js
@@ -0,0 +1,11 @@
+let elementBtn = document.getElementById("btn");
+
+// Click Event Listener
+elementBtn.addEventListener('click', function(){
+     console.log("Click Done!!")
+})
+
+// ContextMenu Event Listener
+// elementBtn.addEventListener('click', function(){
+//      console.log("Click Done!!")
+// })
