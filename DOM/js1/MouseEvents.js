9a7d6ae7d71546f33e75893d00bb6ce3ec1ec48e
Rishab Raj <rr0948555@gmail.com>
2026-03-27 13:26:25 +0530

    MouseEvents

--- DOM/js1/MouseEvents.js
+++ DOM/js1/MouseEvents.js
@@ -1,11 +1,34 @@
 let elementBtn = document.getElementById("btn");
 
 // Click Event Listener
-elementBtn.addEventListener('click', function(){
-     console.log("Click Done!!")
-})
-
-// ContextMenu Event Listener
 // elementBtn.addEventListener('click', function(){
 //      console.log("Click Done!!")
 // })
+
+// ContextMenu Event Listener
+// elementBtn.addEventListener('contextmenu', function(){
+//      console.log("Right Click Done!!")
+// })
+
+
+// // doubleClick Event Listener
+// elementBtn.addEventListener('dbclick', function(){
+//      console.log("Right Click Done!!")
+// })
+
+
+// doubleClick Event Listener
+// elementBtn.addEventListener('dblclick', function(){
+//      console.log("Double Click Done!!")
+// })
+
+
+// mouseover Event Listener
+// elementBtn.addEventListener('mouseover', function(){
+//      console.log("Mouse Over Done!!")
+// })
+
+// mouseover Event Listener
+elementBtn.addEventListener('mouseout', function(){
+     console.log("Mouse Out Done!!")
+})
