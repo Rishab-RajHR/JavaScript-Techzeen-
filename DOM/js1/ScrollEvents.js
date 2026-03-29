1971d70e694a6e41fd2f57f8b6baa320e73fd416
Rishab Raj <rr0948555@gmail.com>
2026-03-29 13:44:10 +0530

    Scroll

--- DOM/js1/ScrollEvents.js
+++ DOM/js1/ScrollEvents.js
@@ -1,3 +1,13 @@
-window.addEventListener('scroll', function(){
-   console.log("Scroll Event Listened!!!");
+// window.addEventListener('scroll', function(){
+//    console.log("Scroll Event Listened!!!");
+// })
+
+
+window.addEventListener('wheel', function(event){
+     if(event.deltaY < 0) {
+        console.log("Scrolling Up!!!");
+     }
+     else if(event.deltaY > 0) {
+        console.log("Scrolling Down!!!");
+     }
 })
