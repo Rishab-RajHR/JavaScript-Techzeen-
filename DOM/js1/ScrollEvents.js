c931ce966b126285a58a7b73be50c0aee2af48e2
Rishab Raj <rr0948555@gmail.com>
2026-03-29 13:48:45 +0530

    Scroll

--- DOM/js1/ScrollEvents.js
+++ DOM/js1/ScrollEvents.js
@@ -3,11 +3,22 @@
 // })
 
 
-window.addEventListener('wheel', function(event){
-     if(event.deltaY < 0) {
-        console.log("Scrolling Up!!!");
-     }
-     else if(event.deltaY > 0) {
-        console.log("Scrolling Down!!!");
-     }
+// window.addEventListener('wheel', function(event){
+//      if(event.deltaY < 0) {
+//         console.log("Scrolling Up!!!");
+//      }
+//      else if(event.deltaY > 0) {
+//         console.log("Scrolling Down!!!");
+//      }
+// })
+
+
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset > 250) {
+        document.body.style.background = "aqua";
+    }
+    else {
+        document.body.style.background = "white";
+    }
 })
