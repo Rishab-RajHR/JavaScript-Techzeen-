3b5a0095a5d19458bf3b11410bdbc1360264ef72
Rishab Raj <rr0948555@gmail.com>
2026-03-30 13:11:19 +0530

    Hello

--- DOM/js1/InputEvents.js
+++ DOM/js1/InputEvents.js
@@ -1,12 +1,17 @@
 let element = document.getElementById("me");
 
 function myFocus(){
-    
+     element.style.background = "aqua";
 }
 
 function myBlur(){
-
+     element.style.background = "white";
 }
+
+function myChange() {
+     console.log(this.value);
+}
+
 
 element.addEventListener('focus', myFocus);
 element.addEventListener('blur', myBlur);
