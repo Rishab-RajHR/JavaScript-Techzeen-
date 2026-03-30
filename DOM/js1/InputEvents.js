4cf847aca8576e020871f808fa5670859e8e5dd1
Rishab Raj <rr0948555@gmail.com>
2026-03-30 13:13:59 +0530

    Input Event

--- DOM/js1/InputEvents.js
+++ DOM/js1/InputEvents.js
@@ -12,6 +12,12 @@ function myChange() {
      console.log(this.value);
 }
 
+function myInput() {
+    console.log(this.value);
+}
+
 
 element.addEventListener('focus', myFocus);
 element.addEventListener('blur', myBlur);
+// element.addEventListener('change', myChange);
+element.addEventListener('input', myInput);
