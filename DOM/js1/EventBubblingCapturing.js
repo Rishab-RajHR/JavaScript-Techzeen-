f598df093451d98eee38197ac92705e0018a62ee
Rishab Raj <rr0948555@gmail.com>
2026-03-31 13:17:53 +0530

    Event

--- DOM/js1/EventBubblingCapturing.js
+++ DOM/js1/EventBubblingCapturing.js
@@ -2,21 +2,27 @@ let divElement = document.getElementById("mainDiv");
 let buttonElement = document.getElementById("btn");
 
 // Default =>(false) Event Bubbling -> child first
-// buttonElement.addEventListener('click', btnClick)
-// divElement.addEventListener('click', divClick)
-// document.body.addEventListener('click', bodyClick)
+buttonElement.addEventListener('click', btnClick)
+divElement.addEventListener('click', divClick)
+document.body.addEventListener('click', bodyClick)
 
 
+
+// For doing Event Bubbling we do not pass true as parameter
 // Event Capturing --> true(capturing)--> parent first
-buttonElement.addEventListener('click', btnClick, true)
-divElement.addEventListener('click', divClick, true)
-document.body.addEventListener('click', bodyClick, true)
+// buttonElement.addEventListener('click', btnClick, true)
+// divElement.addEventListener('click', divClick, true)
+// document.body.addEventListener('click', bodyClick, true)
+
+
+// For stopping the execution we use stipPropogation
 
 function btnClick(){
     console.log("Button Clicked!!!")
 }
-function divClick(){
+function divClick(event){
     console.log("Div Clicked!!!")
+    event.stopPropagation();
 }
 function bodyClick(){
     console.log("Body Clicked!!!")
