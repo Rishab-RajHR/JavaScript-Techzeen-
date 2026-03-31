d0d0aa544dc1dd14a3ab5e0dbafd2db3103abcc6
Rishab Raj <rr0948555@gmail.com>
2026-03-31 13:07:33 +0530

    Events

---
+++ DOM/js1/EventBubblingCapturing.js
@@ -0,0 +1,7 @@
+let divElement = document.getElementById("mainDiv");
+let buttonElement = document.getElementById("btn");
+
+buttonElement.addEventListener('click', btnClick)
+divElement.addEventListener('click', divClick)
+document.body.addEventListener('click', bodyClick)
+
