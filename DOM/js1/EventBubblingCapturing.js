0023186f0d56e22ae5565fbb1c1df45be16bae2f
Rishab Raj <rr0948555@gmail.com>
2026-03-31 13:12:59 +0530

    Event

--- DOM/js1/EventBubblingCapturing.js
+++ DOM/js1/EventBubblingCapturing.js
@@ -2,9 +2,15 @@ let divElement = document.getElementById("mainDiv");
 let buttonElement = document.getElementById("btn");
 
 // Default =>(false) Event Bubbling -> child first
-buttonElement.addEventListener('click', btnClick)
-divElement.addEventListener('click', divClick)
-document.body.addEventListener('click', bodyClick)
+// buttonElement.addEventListener('click', btnClick)
+// divElement.addEventListener('click', divClick)
+// document.body.addEventListener('click', bodyClick)
+
+
+// Event Capturing --> true(capturing)--> parent first
+buttonElement.addEventListener('click', btnClick, true)
+divElement.addEventListener('click', divClick, true)
+document.body.addEventListener('click', bodyClick, true)
 
 function btnClick(){
     console.log("Button Clicked!!!")
