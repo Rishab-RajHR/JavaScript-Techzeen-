b0e89f90d7707e152579fce7c9e7faf7ddb6ea20
Rishab Raj <rr0948555@gmail.com>
2026-04-01 11:04:53 +0530

    PreventDefault

--- DOM/js1/PreventDefault.js
+++ DOM/js1/PreventDefault.js
@@ -1,6 +1,13 @@
-let element = document.getElementById("anchor");
+// let element = document.getElementById("anchor");
 
-element.addEventListener("click", function(e){
-      console.log("Link Clicked")
+// element.addEventListener("click", function(e){
+//       console.log("Link Clicked")
+//       e.preventDefault();
+// })
+
+
+let formElement = document.getElementById("myForm");
+
+formElement.addEventListener("submit", function(e){
       e.preventDefault();
 })
