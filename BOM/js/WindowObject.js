cdd8beecf958d2acf5d844db08c2188afb7566d2
Rishab Raj <rr0948555@gmail.com>
2026-04-03 10:44:46 +0530

    Window

--- BOM/js/WindowObject.js
+++ BOM/js/WindowObject.js
@@ -1,4 +1,22 @@
 // innerWidth innerHeight
 
-console.log(window.innerWidth);
-console.log(window.innerWidth);
+// console.log(window.innerWidth);
+// console.log(window.innerWidth);
+
+
+// outerWidth outerHeight
+
+// console.log(window.outerWidth);
+// console.log(window.outerWidth);
+
+
+// open new window
+let element1 = document.getElementById('btn-1');
+
+let url = "https://www.yahoo.com";
+let features = "height=500, width=500";
+
+element1.addEventListener('click', function(){
+    // first parameter = url, second parameter = name, third paramter = features
+    window.open(URL, 'yahoo', features);
+})
