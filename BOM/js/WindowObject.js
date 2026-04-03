54ff2acab96d6cc715c197e83deb5d75040a15d9
Rishab Raj <rr0948555@gmail.com>
2026-04-03 10:53:37 +0530

    Window Object

--- BOM/js/WindowObject.js
+++ BOM/js/WindowObject.js
@@ -12,11 +12,28 @@
 
 // open new window
 let element1 = document.getElementById('btn-1');
+let element2 = document.getElementById('btn-2');
+let element3 = document.getElementById('btn-3');
 
 let url = "https://www.yahoo.com";
 let features = "height=500, width=500";
 
+let openWindow;
+
 element1.addEventListener('click', function(){
     // first parameter = url, second parameter = name, third paramter = features
-    window.open(URL, 'yahoo', features);
+   openWindow =  window.open(URL, 'web', features);
 })
+
+
+// Change url
+element2.addEventListener('click', function(){
+    window.open('https://www.google.com', 'web');
+})
+
+
+// Close Window
+element3.addEventListener('click', function(){
+    openWindow.close();
+})
+
