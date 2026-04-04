cb31058bd4f37a282b2054206edd99b7ca3f832c
Rishab Raj <rr0948555@gmail.com>
2026-04-04 11:07:33 +0530

    Timing

--- BOM/js/TimingFunc.js
+++ BOM/js/TimingFunc.js
@@ -1,7 +1,13 @@
 // Timeout Function
 
-setTimeout(Show, 3000); // 3000ms --> 3sec
+let timeId = setTimeout(show, 3000); // 3000ms --> 3sec
 
 function show(){
   console.log("Good Morning");
 }
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    clearTimeout(timeId);
+})
