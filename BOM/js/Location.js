2f016d4442d640ef56a0a80a5cb11ce71bb948a4
Rishab Raj <rr0948555@gmail.com>
2026-04-05 10:44:35 +0530

    Location

--- BOM/js/Location.js
+++ BOM/js/Location.js
@@ -2,3 +2,8 @@
 console.log(location.href);
 console.log(location.pathname);
 console.log(location.protocol);
+
+// For Redirecting to URL
+function go(){
+   window.location = "https://www.google.com";
+}
