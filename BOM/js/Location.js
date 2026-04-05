cfb48581a0a91bc07fcc5747ae696a041d8f2907
Rishab Raj <rr0948555@gmail.com>
2026-04-05 10:49:37 +0530

    Locatiion

--- BOM/js/Location.js
+++ BOM/js/Location.js
@@ -5,5 +5,11 @@ console.log(location.protocol);
 
 // For Redirecting to URL
 function go(){
-   window.location = "https://www.google.com";
+  //  window.location = "https://www.google.com";
+  //  For setting the URL
+  location.href = "https://www.google.com";
+
+  // Method for URL Redirection - assign and replace
+  location.assign("https://www.google.com")  // assign maintains history (add in history)
+  location.replace("https://www.google.com") // replace doesn't maintains history (not add in history)
 }
