5665c0330166f4ac4b0902f479b8f14ee45f8360
Rishab Raj <rr0948555@gmail.com>
2026-04-08 10:56:16 +0530

    letconst

--- EcmaScript/js/letconst.js
+++ EcmaScript/js/letconst.js
@@ -11,8 +11,27 @@
 //  onsole.log(window.age1) => undefined in let
 
 
-// re-declaration 
-var a = 10;
-var a;
 
-console.log(a);
+// re-declaration 
+// var a = 10;
+// var a;
+
+// console.log(a); => var can be redeclared
+
+
+// let a2  = 10;
+// let a2;
+
+// console.log(a2); => let cannot be redeclared
+
+
+// const keyword
+// var can be changed after declaring
+// var b = 2;
+// b = 3;
+// console.log(b);
+
+// let can be changed after declaring
+let b = 2;
+b = 3;
+console.log(b);
