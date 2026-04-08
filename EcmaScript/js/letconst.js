d1c977cf04cf005aa4151e6a8df65e414afc24fa
Rishab Raj <rr0948555@gmail.com>
2026-04-08 10:52:23 +0530

    Scope

--- EcmaScript/js/letconst.js
+++ EcmaScript/js/letconst.js
@@ -1,7 +1,18 @@
 // var age = 18; => global scope
 
-// console.log(window.age)
+// console.log(window.age)  => 18 in const
 
- let age1 = 18; //=> block scope
 
- console.log(age1)
+
+
+//  let age1 = 18; //=> block scope
+
+//  console.log(age1)
+//  onsole.log(window.age1) => undefined in let
+
+
+// re-declaration 
+var a = 10;
+var a;
+
+console.log(a);
