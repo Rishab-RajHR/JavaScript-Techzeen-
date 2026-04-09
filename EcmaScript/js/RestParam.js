7a53da959f15cec896d388c2c4e6dc6a11566220
Rishab Raj <rr0948555@gmail.com>
2026-04-09 10:36:31 +0530

    Rest Parameter

--- EcmaScript/js/RestParam.js
+++ EcmaScript/js/RestParam.js
@@ -3,9 +3,14 @@
 // }
 // add(2,5);
 
-// rest parameter
+// rest parameter => makes the array 
 function add(...args){
     let sum = 0;
-    console.log(args);
+    // console.log(args);
+    for(let i=0; i<args.length; i++){
+        sum += args[i];
+    }
+    console.log(sum);
 }
 add(2,3);
+add(1,5,9);
