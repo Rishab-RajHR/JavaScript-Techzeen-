909026c04200e4de7c222575ceb73f7634756e42
Rishab Raj <rr0948555@gmail.com>
2026-04-10 10:30:32 +0530

    ForOf

--- EcmaScript/js/ForofLoop.js
+++ EcmaScript/js/ForofLoop.js
@@ -1,7 +1,13 @@
 let arr = [1,2,3,4,5];
 
 // for loop
-for(let i=0; i<arr.length; i++) 
-{
-    console.log(arr[i]);
+// for(let i=0; i<arr.length; i++) 
+// {
+//     console.log(arr[i]);
+// }
+
+
+// for of loop
+for(let a of arr){
+   console.log(a)
 }
