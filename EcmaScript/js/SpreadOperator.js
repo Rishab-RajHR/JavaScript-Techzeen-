ec96eb50483379ce5f0ec58aea63f9da4a07248c
Rishab Raj <rr0948555@gmail.com>
2026-04-10 10:24:02 +0530

    Spread

--- EcmaScript/js/SpreadOperator.js
+++ EcmaScript/js/SpreadOperator.js
@@ -1,6 +1,17 @@
+// let arr1 = [1,2,3];
+
+// let arr2 = [4,5,6, ...arr1];
+
+// console.log(arr2);
+
+
+// Another method
+
 let arr1 = [1,2,3];
 
-let arr2 = [4,5,6, ...arr1];
+let arr2 = [4,5,6];
 
-console.log(arr2)
+let arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
 
