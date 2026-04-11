57a1e2d0aedb42f265cc46426a7515f47c7a3ee0
Rishab Raj <rr0948555@gmail.com>
2026-04-11 10:21:29 +0530

    Template

--- EcmaScript/js/TemplateLiterals.js
+++ EcmaScript/js/TemplateLiterals.js
@@ -1,5 +1,15 @@
-let str = "Alex Pandian";
+// let str = "Alex Pandian";
+
+// console.log(str);
+
+
+// Template Literals 
+// We can give any number of spaces
+
+let str = `Alex
+
+
+
+Pandian`;
 
 console.log(str);
-
-
