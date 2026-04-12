5725324de670f9bc4fa1415619a2030c739c22e6
Rishab Raj <rr0948555@gmail.com>
2026-04-12 10:24:21 +0530

    Array

--- EcmaScript/js/ArrayDestructing.js
+++ EcmaScript/js/ArrayDestructing.js
@@ -2,10 +2,17 @@ let info = ["Alex", "JS", 21];
 
 // console.log(info);
 
-let name = info[0];
-let fav = info[1];
-let age = info[2];
+// let name = info[0];
+// let fav = info[1];
+// let age = info[2];
+
+
+
+// Array Destructuring
+let [name, fav, age, extra] = info;
+
 
 console.log(name);
 console.log(fav);
 console.log(age);
+console.log(extra);
