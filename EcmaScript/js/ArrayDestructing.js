b0c8457344aee8e82c5c8b88d24ab3185c86b46f
Rishab Raj <rr0948555@gmail.com>
2026-04-12 10:28:33 +0530

    Array

--- EcmaScript/js/ArrayDestructing.js
+++ EcmaScript/js/ArrayDestructing.js
@@ -9,10 +9,23 @@ let info = ["Alex", "JS", 21];
 
 
 // Array Destructuring
-let [name, fav, age, extra] = info;
+// let [name, fav, age, extra] = info;
 
+
+// console.log(name);
+// console.log(fav);
+// console.log(age);
+// console.log(extra); // It will give undefined
+
+
+// Nested Array Concept
+let info1 = ["Alex", "JS", 21, ['Pandain', 2026]];
+
+// nested array destructuring
+let [name, fav, age, [channel, year]] = info1;
 
 console.log(name);
 console.log(fav);
 console.log(age);
-console.log(extra);
+console.log(channel);
+console.log(year);
