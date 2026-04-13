6573489a0ceebc3bba29cda12e1fafa0fd0901a9
Rishab Raj <rr0948555@gmail.com>
2026-04-13 10:37:51 +0530

    Object

--- EcmaScript/js/ObjectDestructuring.js
+++ EcmaScript/js/ObjectDestructuring.js
@@ -10,8 +10,18 @@ let employee = {
 
 
 // With Object Destructuring
-let {name, age, dep} = employee;
+// let {name, age, dep} = employee;
 
-console.log(name)
-console.log(age)
-console.log(dep)
+// console.log(name)
+// console.log(age)
+// console.log(dep)
+
+
+// Change the variable name
+// let {name : firstName, age, dep} = employee;
+// console.log(firstName)
+
+
+// Set to default
+let {name, age=33, dep} = employee;
+console.log(age);
