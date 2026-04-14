b13fba5dcbce94b34788fe1f4721cd3e2ef12e2b
Rishab Raj <rr0948555@gmail.com>
2026-04-14 10:15:50 +0530

    Callback

--- EcmaScript/js/Callback.js
+++ EcmaScript/js/Callback.js
@@ -2,10 +2,16 @@ function greetings(){
      console.log("Good Morning!");
 }
 
+function night(){
+    console.log("Good Night!");
+}
+
 function add(num1, num2, call){
      console.log(num1 + num2);
      call();
 }
+
 // add(1,2);
 add(1, 2, greetings);
+add(1, 2, night);
 
