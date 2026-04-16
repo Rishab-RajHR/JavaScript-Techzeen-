bda264d065d7d6c3cbb119a5d9c7fb113fad376b
Rishab Raj <rr0948555@gmail.com>
2026-04-16 10:30:23 +0530

    Map

--- EcmaScript/js/MapFunction.js
+++ EcmaScript/js/MapFunction.js
@@ -1,8 +1,18 @@
 // Without Map Function
-let arr = [10,30,50,70];
-let result = [];
+// let arr = [10,30,50,70];
+// let result = [];
 
-for(let i=0; i<arr.length; i++){
-      result[i] = arr[i]*2;
-}
+// for(let i=0; i<arr.length; i++){
+//       result[i] = arr[i]*2;
+// }
+// console.log(result);
+
+
+// With the Help of Map Function
+let arr = [10,30,50,70];
+
+// Using anonymous Function
+let result = arr.map(function(num){
+     return num*3;
+});
 console.log(result);
