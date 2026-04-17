f7fa01639372c0b6668a6d4ae54c087c28ad2819
Rishab Raj <rr0948555@gmail.com>
2026-04-17 10:32:46 +0530

    Filter

--- EcmaScript/js/FilteringFunction.js
+++ EcmaScript/js/FilteringFunction.js
@@ -1,7 +1,12 @@
 let arr = [1,22,15,3,6,9,67,98,54,23,87];
 
 // using arrow function 
+// let result = arr.filter(val => val > 10);
 
-let result = arr.filter(val => val > 10);
+// using anonymous function
+
+let result = arr.filter(function(val){
+     return val > 30;
+})
 
 console.log(result);
