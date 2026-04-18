96f1db5b4622ca97aaaab431306e3f767149f868
Rishab Raj <rr0948555@gmail.com>
2026-04-18 10:19:03 +0530

    Reduce

--- EcmaScript/js/Reduce.js
+++ EcmaScript/js/Reduce.js
@@ -1,6 +1,11 @@
 let arr = [1,2,3,4,5];
 
 // using arrow function
-let result = arr.reduce((val1, val2) => val1 + val2);
+// let result = arr.reduce((val1, val2) => val1 + val2);
+
+// using anonymous function
+let result = arr.reduce(function(val1, val2){
+     return val1 + val2;
+})
 
 console.log(result);  // 3 , 6 , 10, 15
