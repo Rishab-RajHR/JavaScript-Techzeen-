ec8b57445ea245877ee0fc34eb4c641abb9a1383
Rishab Raj <rr0948555@gmail.com>
2026-04-19 10:25:39 +0530

    Read Cookie

--- EcmaScript/js/Cookies.js
+++ EcmaScript/js/Cookies.js
@@ -1,3 +1,9 @@
 // If we want to store data in the browser that is called cookies
 document.cookie = "item=phone"; // create cookie
 document.cookie = "item=phone,headphone"; // update cookie
+
+
+// read a cookie
+let cocomo = document.cookie;
+
+console.log(cocomo);
