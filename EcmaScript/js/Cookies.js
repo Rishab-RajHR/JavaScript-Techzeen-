7cb3dcf97b3aa650d476b30500aaba256b1dc13a
Rishab Raj <rr0948555@gmail.com>
2026-04-19 10:29:25 +0530

    Cookies

--- EcmaScript/js/Cookies.js
+++ EcmaScript/js/Cookies.js
@@ -9,4 +9,5 @@ let cocomo = document.cookie;
 console.log(cocomo);
 
 // delete a cookie
-document.cookie = "item=phone,headphones; expires=Thu, 20 April 2026 12:00:00 UTC";
+// If we want to delete the cookies we use the past date
+document.cookie = "item=phone,headphones; expires=Thu, 20 March 2026 12:00:00 UTC";
