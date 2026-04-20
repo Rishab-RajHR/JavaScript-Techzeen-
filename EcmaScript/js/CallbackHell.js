09fdc115b720434988d62caf3ee76c0ba684be05
Rishab Raj <rr0948555@gmail.com>
2026-04-20 10:25:05 +0530

    Callback Hell

--- EcmaScript/js/CallbackHell.js
+++ EcmaScript/js/CallbackHell.js
@@ -1,6 +1,13 @@
 //Synchronous Programming => sequential works (dependent Programming)
-console.log("First")
-console.log("Second")
-console.log("Third")
+// console.log("First")
+// console.log("Second")
+// console.log("Third")
 
 //Asynchronous Programming => It is not dependent on other statment
+console.log("First")
+
+setTimeout(() => {
+    console.log("Second");
+},3000)
+
+console.log("Third")
