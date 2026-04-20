74414f11f21a259bc5fc730dbeacb69558aa3395
Rishab Raj <rr0948555@gmail.com>
2026-04-20 10:23:29 +0530

    Callback Hell

--- EcmaScript/js/CallbackHell.js
+++ EcmaScript/js/CallbackHell.js
@@ -1,3 +1,6 @@
-//Synchronous Programming
+//Synchronous Programming => sequential works (dependent Programming)
+console.log("First")
+console.log("Second")
+console.log("Third")
 
-//Asynchronous Programming
+//Asynchronous Programming => It is not dependent on other statment
