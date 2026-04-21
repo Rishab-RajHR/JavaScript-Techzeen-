09cd6c8beaa77a838a358d6d1bfdb06d64faadf7
Rishab Raj <rr0948555@gmail.com>
2026-04-21 10:02:08 +0530

    ThenCatch

--- EcmaScript/js/ThenCatch.js
+++ EcmaScript/js/ThenCatch.js
@@ -2,6 +2,9 @@ let pr1 = new Promise((resolve, reject) => {
      console.log("Promise Is Pending....");
      setTimeout(()=>{
         console.log("Promise is Fulfilled");
-        resolve(true);
+        // resolve(true);
+        reject(new Error("I'm an Error"));
      }, 5000);
 })
+
+console.log(pr1)
