4bb981347c2b23ab82eec9b4fe27858551628acf
Rishab Raj <rr0948555@gmail.com>
2026-04-21 10:11:03 +0530

    ThenCatch

--- EcmaScript/js/ThenCatch.js
+++ EcmaScript/js/ThenCatch.js
@@ -20,4 +20,10 @@ let pr2 = new Promise((resolve, reject) => {
      }, 5000);
 })
 
+
+pr2.then((value) => {
+    console.log(value);
+}, (error) => {
+    console.log("Error in Promise 2");
+})
 console.log(pr1, pr2);
