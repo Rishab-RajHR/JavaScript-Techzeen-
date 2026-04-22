998e388ec8d4249096652212afe0436943ac92c2
Rishab Raj <rr0948555@gmail.com>
2026-04-22 07:43:00 +0530

    Promise Chaining

--- EcmaScript/js/PromiseChaining.js
+++ EcmaScript/js/PromiseChaining.js
@@ -1,19 +1,33 @@
-let p1 = new Promise((resolve, reject) => {
+// let p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//        console.log("Completed After 3 sec");
+//        resolve(true);
+//     }, 3000);
+// });
+// p1.then((value) => {
+//     console.log(value);
+//     let p2 = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//              resolve("Promise 2 is resolve");
+//         }, 2000);
+//     });
+//     return p2;
+// }).then((value) => {
+//     console.log(value);
+// }).then((value) => {
+//     console.log("confirmed Resolved");
+// })
+
+
+let p = new Promise((resolve, reject) => {
     setTimeout(() => {
-       console.log("Completed After 3 sec");
-       resolve(true);
+        resolve("1");
     }, 3000);
-});
-p1.then((value) => {
-    console.log(value);
-    let p2 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-             resolve("Promise 2 is resolve");
-        }, 2000);
-    });
-    return p2;
+}).then((value) => {
+   return value * 2; // 1
+}).then((value) => { 
+    return value * 2; // 2
 }).then((value) => {
     console.log(value);
-}).then((value) => {
-    console.log("confirmed Resolved");
+    return value * 2; // 4
 })
