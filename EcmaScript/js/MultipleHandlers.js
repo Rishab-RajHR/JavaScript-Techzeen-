365dbbf4bc0fa9a1a9b3d05d086eeb6629ebcf17
Rishab Raj <rr0948555@gmail.com>
2026-04-23 10:46:55 +0530

    Multiple Handlers

---
+++ EcmaScript/js/MultipleHandlers.js
@@ -0,0 +1,10 @@
+let p1 = new Promise((resolve,reject) => {
+    console.log("Promise is not resolved");
+    setTimeout(() => {
+        resolve(1);
+    }, 3000);
+})
+
+p1.then(() => {
+    console.log("Promise is Resolved");
+})
