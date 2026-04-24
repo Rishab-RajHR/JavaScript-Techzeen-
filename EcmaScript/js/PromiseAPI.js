83cc53743b05004c0a593e08b28bf14a9dbbcfb0
Rishab Raj <rr0948555@gmail.com>
2026-04-24 14:58:20 +0530

    Promise API

--- EcmaScript/js/PromiseAPI.js
+++ EcmaScript/js/PromiseAPI.js
@@ -5,7 +5,8 @@ let p1 = new Promise((resolve, reject) => {
 })
 let p2 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(2);
+        // resolve(2);
+        reject(new Error("Error In Promise 2"))
     }, 3000);
 })
 let p3 = new Promise((resolve, reject) => {
@@ -14,12 +15,17 @@ let p3 = new Promise((resolve, reject) => {
     }, 4000);
 })
 
-p1.then((value) => {
-    console.log(value);
-})
-p2.then((value) => {
-    console.log(value);
-})
-p3.then((value) => {
-    console.log(value);
+// p1.then((value) => {
+//     console.log(value);
+// })
+// p2.then((value) => {
+//     console.log(value);
+// })
+// p3.then((value) => {
+//     console.log(value);
+// })
+
+let promise_api = Promise.all([p1, p2, p3]);
+promise_api.then((value) => {
+    console.log(value)
 })
