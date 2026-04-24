b485e8e41979376b40b4a510c2c41d2c3488fb97
Rishab Raj <rr0948555@gmail.com>
2026-04-24 14:49:00 +0530

    Promise

--- EcmaScript/js/PromiseAPI.js
+++ EcmaScript/js/PromiseAPI.js
@@ -14,6 +14,12 @@ let p3 = new Promise((resolve, reject) => {
     }, 4000);
 })
 
-pt.then((value) => {
+p1.then((value) => {
+    console.log(value);
+})
+p2.then((value) => {
+    console.log(value);
+})
+p3.then((value) => {
     console.log(value);
 })
