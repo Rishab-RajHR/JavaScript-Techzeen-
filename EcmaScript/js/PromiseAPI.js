8bd658376de253c63f943bcb021ccb9b4529ce83
Rishab Raj <rr0948555@gmail.com>
2026-04-24 14:44:03 +0530

    Promise

---
+++ EcmaScript/js/PromiseAPI.js
@@ -0,0 +1,15 @@
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(1);
+    }, 2000);
+})
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(1);
+    }, 2000);
+})
+let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(1);
+    }, 2000);
+})
