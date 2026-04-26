46fb6b24e6f9463dfb804aa574d6d8ddb16fa0af
Rishab Raj <rr0948555@gmail.com>
2026-04-26 10:09:17 +0530

    LocalStorage

--- EcmaScript/js/LocalStorage.js
+++ EcmaScript/js/LocalStorage.js
@@ -6,3 +6,8 @@ localStorage.setItem(key, value);
 
 // getItem
 console.log(`The value ${key} is ${localStorage.getItem(key)}`)
+
+//  removeItem
+if(key == "password"){
+    localStorage.removeItem(key);
+}
