70e7f512babb0608068a1cad9323cc982a3c56c4
Rishab Raj <rr0948555@gmail.com>
2026-04-28 09:56:12 +0530

    OOPS

---
+++ OOPS/js/oops.js
@@ -0,0 +1,23 @@
+/*********************** Object Oriented Programming (OOP) *********************
+One of the most common strategies in programming is to solve an issue by creating objects.
+
+DRY-----> Don't Repeat YourSelf
+
+******************* Object ***********************
+ Anything in the world (Living and non-living thing)
+ An Object is a  member also known as instance of a class.
+ Each Object has an:
+ identity, behaviour(actions), and a state(features / properties / attributes).
+
+ ****************** Class ********************
+ Blueprint to create objects.
+ Contains information to create a valid objects
+
+ state of object ----> variables
+ behaviour of object -----> methods (functions)
+
+ *****************  How To Model a Problem In OOPs ********************
+ Identity----> Class -----> Employee
+ State----> Attributes -----> empName, empAge, empSalary;
+ 
+*/
