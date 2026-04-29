71d41da1260da861bf8fb11009770183157a5e52
Rishab Raj <rr0948555@gmail.com>
2026-04-29 10:27:33 +0530

    OOPS

--- OOPS/js/oops2.js
+++ OOPS/js/oops2.js
@@ -1,15 +1,20 @@
 class Form{
      submit(){
-        alert("Form Submitted!!");
+        alert(this.name + ": Form Submitted!!");
      }
      cancel(){
-        alert("Form is not submitted!!");
+        alert(this.name + ": Form is not submitted!!");
+     }
+     fillInfo(givenName){
+        this.name = givenName;
      }
 }
 
 let alex = new Form();
+alex.fillInfo("Alex");
 alex.submit();
 let basil = new Form();
+basil.fillInfo("Basil");
 basil.submit();
 
 // Pascal Convention
