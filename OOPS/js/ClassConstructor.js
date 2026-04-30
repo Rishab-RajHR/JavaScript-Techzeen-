22720183c45555c7a985c585aa04b4a647268dc4
Rishab Raj <rr0948555@gmail.com>
2026-04-30 10:01:54 +0530

    Class Constructor

---
+++ OOPS/js/ClassConstructor.js
@@ -0,0 +1,9 @@
+/* 
+********************** Constructors in JavaScript ***********************
+The constructor() method is a special method for creating and initializing objects created within a class.
+
+The constructor() method is called automatically when a class is initiated, and it has to have the exact name "contructor", in fact, if you do not have a constructor method, JavaScript will add an invisible and empty contructor method.
+
+Note: A class cannot have more than one constructor() method. This will throw a SyntaxError.
+
+*/
