d6adf688d9643ae496dc5aedadd85d1fbe8c6197
Rishab Raj <rr0948555@gmail.com>
2026-05-01 09:37:33 +0530

    Inheritance

---
+++ OOPS/js/Inheritance.js
@@ -0,0 +1,11 @@
+/*
+********************* Inheritance In JavaScript ***********************
+Inheritance enables you to define a class that takes all the functionality from a parent class and allows you to add more.
+
+Using class inheritance, a class can inherit all the methods and properties of another class.
+
+Inheritance is a useful feature that allows code reusability.
+
+To use class inheritance, you use the extends keyword.
+
+*/
