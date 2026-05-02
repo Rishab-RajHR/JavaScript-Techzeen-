8c56ee009d733a2273c620354fbb31e088c6108b
Rishab Raj <rr0948555@gmail.com>
2026-05-02 09:43:57 +0530

    Method Overriding

--- OOPS/js/MethodOverriding.js
+++ OOPS/js/MethodOverriding.js
@@ -1,11 +1,13 @@
-class Person{
+class Parent{
     sayName(name){
         console.log("My Name is: " + name);
     }
 }
 
 class Child extends Parent {
-    
+      sayName(name){
+         console.log("Mera Naam " + name + " hai");
+      }
 }
 
 let c = new Child();
