b9fb2ed3d1056ee3bfa567d879465f4557d4bbc8
Rishab Raj <rr0948555@gmail.com>
2026-05-03 09:58:27 +0530

    Static Method

--- OOPS/js/Static.js
+++ OOPS/js/Static.js
@@ -5,11 +5,12 @@ They are called on the class itself, not on an instance of the class .
 
 class Employee{
    constructor(name){
-      this.name = name;
+      // this.name = name;
+      this.name = Employee.capitalized(name);
    }
    intro(){
-      // console.log(`My Name is ${this.name}`);
-      console.log(`My Name is ${Employee.capitalized(this.name)}`);
+      console.log(`My Name is ${this.name}`);
+      // console.log(`My Name is ${Employee.capitalized(this.name)}`);
    }
    static capitalized(name){
        return name.charAt(0).toUpperCase() + name.substr(1, name.length);
