a7040bb2c0cb44b2cc6b03c806ff1d06bd2e900e
Rishab Raj <rr0948555@gmail.com>
2026-05-03 09:48:00 +0530

    Static Method

---
+++ OOPS/js/Static.js
@@ -0,0 +1,4 @@
+/*  ****************** Static Method in JavaScript ***************************
+Static methods in JavaScript are associated with a class rather than an instance of the class.
+They are called on the class itself, not on an instance of the class .
+*/
