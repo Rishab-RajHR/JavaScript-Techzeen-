16c827e757d803923fff5f3fdc3965e6dad31a2c
Rishab Raj <rr0948555@gmail.com>
2026-05-03 09:51:48 +0530

    Static Method

--- OOPS/js/Static.js
+++ OOPS/js/Static.js
@@ -2,3 +2,15 @@
 Static methods in JavaScript are associated with a class rather than an instance of the class.
 They are called on the class itself, not on an instance of the class .
 */
+
+class Employee{
+   constructor(name){
+      this.name = name;
+   }
+   intro(){
+      console.log(`My Name is ${this.name}`);
+   }
+}
+
+let a  = new Employee("Tovino");
+a.intro();
