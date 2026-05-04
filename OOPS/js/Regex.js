8e94a99e18fa5d2e992dd1498fe487ae69e54449
Rishab Raj <rr0948555@gmail.com>
2026-05-04 09:46:50 +0530

    Regex

--- OOPS/js/Regex.js
+++ OOPS/js/Regex.js
@@ -1,2 +1,5 @@
+let regEx = /very/g; // change globally
 let message = "I'm a very very very good boy and very nice";
-console.log(message.replace('very', 'VERY'));
+
+// console.log(message.replace('very', 'VERY')); // only replace first occurence
+console.log(message.replace(regEx, 'VERY'));
