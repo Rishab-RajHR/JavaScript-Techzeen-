f0afe8d93d43c536d9ab8561275c1f9afe15f395
Rishab Raj <rr0948555@gmail.com>
2026-05-06 09:46:47 +0530

    Parse JSON

---
+++ OOPS/js/parseJSON.js
@@ -0,0 +1,26 @@
+let data = `
+{
+    "name" : "Alex",
+    "age" : 23,
+    "is_employee" : true,
+    "passport" : null,
+     "language" : ["JavaScript", "C++", "Java", "Python"],
+     "contact" : {
+        "phone_number" : 123456789,
+        "email" : "alex12@gmail.com"
+     },
+     "students" : [
+        {
+            "name" : "Naveen",
+            "class" : "Nine"
+        },
+        {
+            "name" : "Tovino",
+            "class" : "Ten"
+        },
+        {
+            "name" : "Basil",
+            "class" : "Eight"
+        }
+     ]
+}`;
