2bdc8474c0eecb0644582c1d8e3037cc2f3cac34
Rishab Raj <rr0948555@gmail.com>
2026-05-08 09:39:12 +0530

    Ajax

--- OOPS/js/Ajax.js
+++ OOPS/js/Ajax.js
@@ -15,3 +15,16 @@
 *) Status Code --> 200(ok) , 404(not found)...
 *) ResponseText or ResponseXML --> type of response
 */
+
+function loadData(){
+   let xhttp = new XMLHttpRequest();
+
+   xhttp.onreadystatechange = function(){
+       if(this.readyState == 4 && this.status == 200){
+          console.log(this.response);
+       }
+   }
+
+   xhttp.open("GET","../file.txt",true);
+   xhttp.send();
+}
