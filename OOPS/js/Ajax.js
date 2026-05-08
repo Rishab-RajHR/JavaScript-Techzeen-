4b5d18fa24025aaeb2fec6b253329044242143b6
Rishab Raj <rr0948555@gmail.com>
2026-05-08 09:26:58 +0530

    AJAX

---
+++ OOPS/js/Ajax.js
@@ -0,0 +1,17 @@
+/*  ******************* Ajax In JavaScript ************************ 
+*) AJAX stands for  --> Asynchronous JavaScript and XML
+*) Definition --> technique for creating fast and dynamic web pages.
+*) How it works --> allows data to be exchanged with a web server in the background, enabling parts of a web page to be updated without requiring a full page reload.
+
+*) What is XMLHttpRequest --> XMLHttpRequest is a JavaScript object that provides an easy way to make asynchronous HTTP requests to a server. It allows you to send and receive data between a web browser and a web server without having to reload the entire page.
+
+*) 5 steps :- 
+     0 Step --> request not initialized
+     1 step --> server connection established
+     2 step --> sent request
+     3 step --> processing request
+     4 step --> request finished and response is ready
+
+*) Status Code --> 200(ok) , 404(not found)...
+*) ResponseText or ResponseXML --> type of response
+*/
