42ac5b52c2062ea408fb44d14415ae461abf214e
Rishab Raj <rr0948555@gmail.com>
2026-05-09 10:15:10 +0530

    Fetch API

--- OOPS/js/FetchAPI.js
+++ OOPS/js/FetchAPI.js
@@ -1,15 +1,27 @@
+// function getData(){
+//     url = './file.txt';
+//     console.log("getData Started")
+//     fetch(url).then((response) => {
+//       console.log("In first .then")
+//         return response.text();
+//     }).then((data) => {
+//         console.log("In second .then")
+//         console.log(data);
+//     })
+// }
+
+// console.log("Before running getData")
+// getData();
+// console.log("After running getData")
+
 function getData(){
-    url = './file.txt';
+    url = 'https://jsonplaceholder.typicode.com/user';
     console.log("getData Started")
     fetch(url).then((response) => {
-      console.log("In first .then")
+        console.log("In first .then")
         return response.text();
     }).then((data) => {
         console.log("In second .then")
         console.log(data);
     })
 }
-
-// console.log("Before running getData")
-// getData();
-// console.log("After running getData")
