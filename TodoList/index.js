4d5b9ed247ada67e21a5fdbabc76d1c4cdc99043
Rishab Raj <rr0948555@gmail.com>
2026-05-13 09:54:33 +0530

    Todo

--- TodoList/index.js
+++ TodoList/index.js
@@ -15,3 +15,12 @@ function addTask(){
     }
     inputElement.value = '';
 }
+
+listElement.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+       e.target.parentElement.remove();
+    }
+}, false);
