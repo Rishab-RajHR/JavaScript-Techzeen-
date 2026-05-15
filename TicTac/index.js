74bbd7ea71d20b64d2c2f09a57d2966e5b719524
Rishab Raj <rr0948555@gmail.com>
2026-05-15 09:48:58 +0530

    TicTac

--- TicTac/index.js
+++ TicTac/index.js
@@ -2,3 +2,7 @@ let playerText = document.getElementById('playerTxt');
 let restartBtn = document.getElementById('restartBtn');
 
 let winningBlock = getComputedStyle(document.body).getPropertyValue('--winning-block');
+
+let boxes = Array.from(document.getElementsByClassName('box'));
+
+console.log(boxes);
