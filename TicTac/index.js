d437b8b88d7fb9e1b2528051fee03bc2da53ad6e
Rishab Raj <rr0948555@gmail.com>
2026-05-16 09:32:14 +0530

    TicTac

--- TicTac/index.js
+++ TicTac/index.js
@@ -37,3 +37,14 @@ function boxClicked(e){
         currentPlayer = currentPlayer == X_Text ? O_Text : X_Text;
      }
 }
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
