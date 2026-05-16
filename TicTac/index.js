f8fc2170cd9bbe5ef0e7dff3e73b108200d5aede
Rishab Raj <rr0948555@gmail.com>
2026-05-16 09:42:01 +0530

    TicTac

--- TicTac/index.js
+++ TicTac/index.js
@@ -59,3 +59,14 @@ function playerHasWon(){
       }
       return false;
 }
+restartBtn.addEventListener('click', restart);
+function restart(){
+      spaces.fill(null);
+      boxes.forEach(box => {
+          box.innerText = '';
+          box.style.backgroundColor = ''
+      });
+      playerText.innerText = 'Tic Tac Toe';
+      currentPlayer = X_Text;
+      startGame();
+}
