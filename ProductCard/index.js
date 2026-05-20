16ae73bada8b262bc08278d22943db301f28092a
Rishab Raj <rr0948555@gmail.com>
2026-05-20 09:32:55 +0530

    Product

--- ProductCard/index.js
+++ ProductCard/index.js
@@ -1,3 +1,8 @@
 let filterButtons = document.querySelectorAll('.filter-buttons button');
 let filterableCards = document.querySelectorAll('.filterable-cards .card');
 console.log(filterButtons,filterableCards);
+
+let filterCards = (e) => {
+    document.querySelector('.active').classList.remove('active')
+    e.target.classList.add('active');
+}
