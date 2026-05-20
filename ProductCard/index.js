c1e62f35d0d049a06bb67c7543e176021041ed0c
Rishab Raj <rr0948555@gmail.com>
2026-05-20 09:30:02 +0530

    Product Card

---
+++ ProductCard/index.js
@@ -0,0 +1,3 @@
+let filterButtons = document.querySelectorAll('.filter-buttons button');
+let filterableCards = document.querySelectorAll('.filterable-cards .card');
+console.log(filterButtons,filterableCards);
