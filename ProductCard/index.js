cb997bf44836ac38d22c25886c3bd1ec44ea51c6
Rishab Raj <rr0948555@gmail.com>
2026-05-20 09:37:13 +0530

    Product

--- ProductCard/index.js
+++ ProductCard/index.js
@@ -5,4 +5,11 @@ console.log(filterButtons,filterableCards);
 let filterCards = (e) => {
     document.querySelector('.active').classList.remove('active')
     e.target.classList.add('active');
+
+    filterableCards.forEach(card => {  
+        card.classList.add('hide');
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === 'all'){
+             card.classList.remove('hide');
+        } 
+    })
 }
