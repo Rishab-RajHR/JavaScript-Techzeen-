21780360990fc3f46f8dc21bf28e7c6d375fe8f5
Rishab Raj <rr0948555@gmail.com>
2026-05-22 09:40:13 +0530

    Notes App

--- NotesApp/index.js
+++ NotesApp/index.js
@@ -11,3 +11,9 @@ createBtn.addEventListener("click", ()=>{
     notesContainer.appendChild(inputBox).appendChild
     (img);
 })
+
+notesContainer.addEventListener("click", function(e){
+     if(e.target.tagName === "IMG"){
+         e.target.parentElement.remove();
+     }
+})
