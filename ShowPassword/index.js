7c7068a067f47e24272312a3db7d687d2e056acc
Rishab Raj <rr0948555@gmail.com>
2026-05-23 09:45:55 +0530

    ShowPasswrod

--- ShowPassword/index.js
+++ ShowPassword/index.js
@@ -4,7 +4,9 @@ let password = document.getElementById('password');
 eyeicon.onclick = function(){
     if(password.type == "password"){
         password.type = "text";
+        eyeicon.src = "images/open-eye.png";
     }else{
         password.type = "password";
+        eyeicon.src = "images/eye-closed.png";
     }
 }
