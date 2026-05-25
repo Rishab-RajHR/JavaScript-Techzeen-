7d4fd51bb899ae5cde0dcc5a0783c5c023358b22
Rishab Raj <rr0948555@gmail.com>
2026-05-25 22:33:19 +0530

    Drag And Drop using HTML CSS JS

--- DragDrop/script.js
+++ DragDrop/script.js
@@ -8,6 +8,17 @@ for(list of lists){
 
         rightBox.addEventListener("dragover", function(e){
             e.preventDefault();
-        })
+        });
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        });
     })
 }
