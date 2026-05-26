c360848345df6ec726357f2cafb6b6af60e67baf
Rishab Raj <rr0948555@gmail.com>
2026-05-26 10:09:51 +0530

    Digital Clock

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -5,3 +5,7 @@ let sec = document.getElementById("sec");
 let currentTime = new Date();
 
 console.log(currentTime);
+
+hrs.innerHTML = currentTime.getHours();
+min.innerHTML = currentTime.getMinutes();
+sec.innerHTML = currentTime.getSeconds();
