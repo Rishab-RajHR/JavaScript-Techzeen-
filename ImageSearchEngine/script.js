d96ec7e5621f79dd33b96ccc13826c96c2878649
Rishab Raj <rr0948555@gmail.com>
2026-05-30 19:15:10 +0530

    Image

--- ImageSearchEngine/script.js
+++ ImageSearchEngine/script.js
@@ -1,14 +1,15 @@
+const accessKey = "LJJFgx2C-G8tv_HsjGR6TJ31MfbzKCnpS4m_GCBBj1s";
+
 const searchBtn = document.getElementById("search-btn");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const searchMoreBtn = document.getElementById("show-more-btn");
 
-LJJFgx2C-G8tv_HsjGR6TJ31MfbzKCnpS4m_GCBBj1s
 
 let keyword = "";
 let page  = 1;
 
 async function searchImages(){
      keyword = searchBox.ariaValueMax;
-     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}`;
+     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 }
