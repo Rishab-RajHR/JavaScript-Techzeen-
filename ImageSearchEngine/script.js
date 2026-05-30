280011a7412ab2d63357c46f7ffc5273bd6287a0
Rishab Raj <rr0948555@gmail.com>
2026-05-30 18:59:51 +0530

    Image Search Engine

---
+++ ImageSearchEngine/script.js
@@ -0,0 +1,4 @@
+const searchBtn = document.getElementById("search-btn");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchMoreBtn = document.getElementById("show-more-btn");
