4849f8803fd0d3885e599addbbe26ee2832c20c1
Rishab Raj <rr0948555@gmail.com>
2026-05-30 19:13:04 +0530

    Image Search Engine

--- ImageSearchEngine/script.js
+++ ImageSearchEngine/script.js
@@ -2,3 +2,13 @@ const searchBtn = document.getElementById("search-btn");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const searchMoreBtn = document.getElementById("show-more-btn");
+
+LJJFgx2C-G8tv_HsjGR6TJ31MfbzKCnpS4m_GCBBj1s
+
+let keyword = "";
+let page  = 1;
+
+async function searchImages(){
+     keyword = searchBox.ariaValueMax;
+     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}`;
+}
