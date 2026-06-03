c88380ee488e6e503168098adb138e4dfef92766
Rishab Raj <rr0948555@gmail.com>
2026-06-03 09:11:10 +0530

    QRCode

--- QRCode/script.js
+++ QRCode/script.js
@@ -3,5 +3,5 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQR(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150X150&data=" + qrText.value;
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + qrText.value;
 }
