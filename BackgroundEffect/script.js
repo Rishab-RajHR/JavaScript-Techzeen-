0b0f2bab308a304ea5d85506b1e365f1a4b1c3bf
Rishab Raj <rr0948555@gmail.com>
2026-06-07 08:54:24 +0530

    BackgroundEffect

--- BackgroundEffect/script.js
+++ BackgroundEffect/script.js
@@ -1,8 +1,12 @@
 var imgBox = document.querySelector(".img-box");
 var imgWrap = document.querySelector(".img-wrap");
+var originalImg = document.getElementById("originalImg");
+
+originalImg.style.width = ImgBox.offsetWidth;
 
 var leftSpace = imgBox.offsetLeft;
 
 imgBox.onmousemove = function(e){
     var boxWidth = (e.pageX - leftSpace) + "px";
+    imgWrap.style.width = boxWidth;
 }
