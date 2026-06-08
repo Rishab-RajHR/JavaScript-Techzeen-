0bd28d1174ae47a91f3733c3566242d80cdc74c1
Rishab Raj <rr0948555@gmail.com>
2026-06-08 09:30:36 +0530

    RGB

--- RGBColorSlider/script.js
+++ RGBColorSlider/script.js
@@ -3,4 +3,5 @@ function colors(){
     var green = document.getElementById("green").value;
     var blue = document.getElementById("blue").value;
     document.body.style.backgroundColor='rgb('+red+','+green+','+blue+')';
+    document.getElementById("output").innerHTML = 'rgb('+red+','+green+','+blue+')';
 }
