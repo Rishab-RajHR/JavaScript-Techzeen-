f532fda38d3cb3d76af42c15af2dcd714d39e88e
Rishab Raj <rr0948555@gmail.com>
2026-06-10 09:46:01 +0530

    Random

---
+++ RandomPassword2/script.js
@@ -0,0 +1,7 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let output = document.getElementById("output");
+
+function genPassword(){
+    let length = document.getElementById('length').nodeValue;
+    document.getElementById("");
+}
