5af38e6fd26980d5b3d9a0af1bbb1fa61167501c
Rishab Raj <rr0948555@gmail.com>
2026-06-10 09:52:04 +0530

    Random

--- RandomPassword2/script.js
+++ RandomPassword2/script.js
@@ -1,7 +1,16 @@
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 let output = document.getElementById("output");
 
+function randomValue(value){
+    return Math.floor(Math.random()*value);
+}
+
 function genPassword(){
-    let length = document.getElementById('length').nodeValue;
-    document.getElementById("");
+    let length = document.getElementById('length').value;
+    document.getElementById("length-val").textContent = length;
+    let str = '';
+
+    for(let i=0; i<length; i++){
+       let random = randomValue(characters.length);
+    }
 }
