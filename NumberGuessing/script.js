ae8f18999df9a4e93ec4408bf9b0c23f9b922e6e
Rishab Raj <rr0948555@gmail.com>
2026-06-12 09:03:37 +0530

    Number Guessing

--- NumberGuessing/script.js
+++ NumberGuessing/script.js
@@ -8,4 +8,19 @@ var guessed_nums = [];
 
 function play(){
     var user_guess = document.getElementById("guess").value;
+    if(user_guess < 1 || user_guess > 100){
+        alert("Please Enter a number between 1 and 100.");
+    }
+    else{
+        guessed_nums.push(user_guess);
+        no_of_guesses += 1;
+
+        if(user_guess < answer){
+            msg1.textContent = "Your guess is too low.";
+            msg2.textContent = "No";
+        }
+        else if(user_guess > answer){
+            
+        }
+    }
 }
