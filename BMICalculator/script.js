c8d6f27a07a8b3c0bda7e957fb0d94b8430def28
Rishab Raj <rr0948555@gmail.com>
2026-06-15 09:07:23 +0530

    Height

--- BMICalculator/script.js
+++ BMICalculator/script.js
@@ -2,4 +2,7 @@ function calculate(){
 
      var weight = parseInt(document.getElementById("Weight").value);
      document.getElementById("weight-val").textContent = weight + " kg";
+
+     var height = parseInt(document.getElementById("height").value);
+     document.getElementById("height-val").textContent = height + " cm";
 }
