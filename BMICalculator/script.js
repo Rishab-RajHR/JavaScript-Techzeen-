995a4cf3be5690d1ef8c77d02c386667ce723209
Rishab Raj <rr0948555@gmail.com>
2026-06-15 09:23:19 +0530

    BMI

--- BMICalculator/script.js
+++ BMICalculator/script.js
@@ -3,7 +3,7 @@ function calculate(){
     var bmi;
     var result = document.getElementById("result");
 
-     var weight = parseInt(document.getElementById("Weight").value);
+     var weight = parseInt(document.getElementById("weight").value);
      document.getElementById("weight-val").textContent = weight + " kg";
 
      var height = parseInt(document.getElementById("height").value);
@@ -14,15 +14,19 @@ function calculate(){
 
      if(bmi < 18.5){
          category = "Underweight";
+         result.style.color = "#ffc44d";
      }
      else if( bmi >= 18.5 && bmi <= 24.9){
          category = "Normal Weight";
+         result.style.color = "#0be881";
      }
      else if( bmi >= 25 && bmi <= 29.9){
         category = "Overweight";
+        result.style.color = "#ff884d";
      }
      else{
         category = "Obese";
+        result.style.color = "#ff5e57";
      }
      document.getElementById("category").textContent = category;
 
