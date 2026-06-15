c06a0a6218449ecd3850b443a9d08fb68ec66c0f
Rishab Raj <rr0948555@gmail.com>
2026-06-15 09:12:35 +0530

    BMI

--- BMICalculator/script.js
+++ BMICalculator/script.js
@@ -1,8 +1,14 @@
 function calculate(){
 
+    var bmi;
+    var result = document.getElementById("result");
+
      var weight = parseInt(document.getElementById("Weight").value);
      document.getElementById("weight-val").textContent = weight + " kg";
 
      var height = parseInt(document.getElementById("height").value);
      document.getElementById("height-val").textContent = height + " cm";
+
+     bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+
 }
