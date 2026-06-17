c4ff5142ad9a4e20da13c5e717cf9ade5147ac4e
Rishab Raj <rr0948555@gmail.com>
2026-06-17 09:21:02 +0530

    StopWatch

--- StopWatch2/script.js
+++ StopWatch2/script.js
@@ -12,6 +12,12 @@ function displayTimer(){
         seconds++;
         if(seconds == 60){
            seconds = 0;
+           minutes++;
+           if(minutes == 60){
+              minutes = 0;
+              hours++;
+           }
         }
      }
+     let h = hours < 10 ? "0" + hours : hours;
 }
