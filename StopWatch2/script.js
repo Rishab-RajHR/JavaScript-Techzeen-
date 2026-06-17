dc60df9f89c297d0fbd8d0e543d146d28260c37c
Rishab Raj <rr0948555@gmail.com>
2026-06-17 09:17:25 +0530

    StopWatch

--- StopWatch2/script.js
+++ StopWatch2/script.js
@@ -1,2 +1,6 @@
 let [milliseconds,seconds,minute,hours] = [0,0,0,0];
 let timerRef = document.querySelector('.timerDisplay');
+
+document.getElementById('startTimer').addEventListener('click', ()=>{
+    int = setInterval(displayTimer,10);
+})
