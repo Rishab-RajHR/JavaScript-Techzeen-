cda65f739d41147689a777c3a7e1c41dca924b9a
Rishab Raj <rr0948555@gmail.com>
2026-06-20 07:56:21 +0530

    Tip Calculator

--- TipCalculator/script.js
+++ TipCalculator/script.js
@@ -12,4 +12,9 @@ function calculateTip(){
      let noOfPeople = document.getElementById("no-of-people").value;
 
      billInput.value = bill.toFixed(2);
+
+     let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+     let total = parseFloat((bill + totalTip).toFixed(2));
+
+     console.log(totalTip, total);
 }
