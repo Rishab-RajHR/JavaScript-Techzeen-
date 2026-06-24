2fd27173b9557885127b5e0d5dcc7fa7a139413f
Rishab Raj <rr0948555@gmail.com>
2026-06-24 09:36:06 +0530

    Age Calulator

--- AgeCalculator/script.js
+++ AgeCalculator/script.js
@@ -3,4 +3,11 @@ const months = [31,28,31,30,31,30,31,31,30,31,30,31];
 function ageCalculate(){
    let today = new Date();
    let inputDate = new Date(document.getElementById("date-input").value);
+   let birthMonth,birthDate,birthYear;
+
+   let birthDetails = {
+       date:inputDate.getDate(),
+       month:inputDate.getMonth(),
+       year:inputDate.getFullYear()
+   }
 }
