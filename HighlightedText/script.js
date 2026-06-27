cdd5fe3c1c0719cdd9f40bc076a240d97dc7f4d1
Rishab Raj <rr0948555@gmail.com>
2026-06-27 10:27:56 +0530

    Highlight Text

---
+++ HighlightedText/script.js
@@ -0,0 +1,6 @@
+function search(){
+    let textToSearch = document.getElementById("text-to-search").ariaValueMax;
+    let paragraph = document.getElementById("paragraph");
+
+    console.log(textToSearch);
+}
