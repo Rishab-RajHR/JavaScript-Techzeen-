763309d0f78345507752457e1b295330edc8a3e2
Rishab Raj <rr0948555@gmail.com>
2026-06-28 10:03:28 +0530

    RGB HEX

--- RGBHEX/script.js
+++ RGBHEX/script.js
@@ -1,3 +1,21 @@
 let hexInput = document.getElementById("hex");
 let rgbInput = document.getElementById("rgb");
-console.log(hexInput, rgbInput);
+
+window.onload=()=>{
+   hexInput.value = "";
+   rgbInput.value = "";
+}
+
+function valid(element){
+     element.style.color = "#202040";
+}
+
+function invalid(element,otherElement){
+    element.style.color = "#f04624";
+    otherElement.value = 0;
+}
+
+function toRgb(){
+    let hexCode = hexInput.value;
+    console.log(hexInput);
+}
