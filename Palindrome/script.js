756b2117527601528f44ed1ab773d2f0fa6c6814
Rishab Raj <rr0948555@gmail.com>
2026-07-01 09:37:17 +0530

    palindrome

--- Palindrome/script.js
+++ Palindrome/script.js
@@ -1,4 +1,9 @@
 document.getElementById("btn").addEventListener("click", function(){
     let txt = document.getElementById("input-text").value;
-    console.log(txt);
-})
+    checkPalindrome(txt);
+});
+
+function checkPalindrome(str){
+    let txt_new = txt.replace(/[^a-zA-Z0-9]/g, '');
+    console.log(txt,txt_new);
+}
