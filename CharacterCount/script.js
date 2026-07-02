8682434a268b42ca89103b3a71dba1b9b5a535c7
Rishab Raj <rr0948555@gmail.com>
2026-07-02 09:36:22 +0530

    Character Count

--- CharacterCount/script.js
+++ CharacterCount/script.js
@@ -2,4 +2,5 @@ let myText = document.getElementById("my-Text");
 myText.addEventListener("input", () => {
     let count = (myText.value).length;
     console.log(myText.value,count);
+    document.getElementById("count-result").textContent = `Total Characters: ${count}`;
 });
