25312865cc353bfac4c93802e33b9e3c33fea0f7
Rishab Raj <rr0948555@gmail.com>
2026-07-05 10:13:56 +0530

    Double Range

--- DoubleRange/script.js
+++ DoubleRange/script.js
@@ -7,13 +7,16 @@ let sliderOne = document.getElementById("slider-1");
 let sliderTwo = document.getElementById("slider-2");
 let displayValOne = document.getElementById("range1");
 let displayValTwo = document.getElementById("range2");
-let minGap = 5;
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
 
 function slideOne(){
    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
      sliderOne.value = parseInt(sliderTwo.value) - minGap;
    }
    displayValOne.textContent = sliderOne.value;
+   fillColor();
 }
 
 function slideTwo(){
@@ -21,4 +24,5 @@ function slideTwo(){
      sliderTwo.value = parseInt(sliderOne.value) + minGap;
   }
   displayValTwo.textContent = sliderTwo.value;
+  fillColor();
 }
