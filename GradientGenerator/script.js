7b1c6127a820015041d11d4968063d10abee5ea5
Rishab Raj <rr0948555@gmail.com>
2026-07-08 09:23:33 +0530

    Gradient Generator

---
+++ GradientGenerator/script.js
@@ -0,0 +1,9 @@
+let colorOne = document.getElementById('color-a');
+let colorTwo = document.getElementById('color-b');
+let currentDirection = 'to bottom';
+let outputCode = document.getElementById('code');
+
+function setDirection(value,_this){
+    let directions = document.querySelectorAll(".buttons button");
+    console.log(directions);
+}
