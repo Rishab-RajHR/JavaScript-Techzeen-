c9069cd8580436b90367f639979b187904d69b08
Rishab Raj <rr0948555@gmail.com>
2026-07-10 09:47:17 +0530

    Analog Watch

--- AnalogWatch/script.js
+++ AnalogWatch/script.js
@@ -9,5 +9,11 @@ let set_clock = setInterval(() => {
      let min = date_now.getMinutes();
      let sec = date_now.getSeconds();
 
-     console.log(hr,min,sec);
+     let calc_hr = (hr * 30) + (min / 2);
+     let calc_min = (min * 6) + (sec / 10);
+     let calc_sec = sec * 6;
+
+     hour.style.transform = `rotate(${calc_hr}deg)`;
+     minute.style.transform = `rotate(${calc_min}deg)`;
+     seconds.style.transform = `rotate(${calc_sec}deg)`;
 }, 1000);
