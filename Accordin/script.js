024af45e99dd32652c58897b8f9d4862c332bd52
Rishab Raj <rr0948555@gmail.com>
2026-07-12 10:11:19 +0530

    Accordin

---
+++ Accordin/script.js
@@ -0,0 +1,5 @@
+let toggles = document.getElementsByClassName('toggle');
+let contentDiv = document.getElementsByClassName('content');
+let icons = document.getElementsByClassName('icon');
+
+console.log(toggles,contentDiv,icons);
