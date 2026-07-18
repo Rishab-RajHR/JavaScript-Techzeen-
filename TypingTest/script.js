9deddf0df7a21faa4185c432e4fa5ae1e1efe634
Rishab Raj <rr0948555@gmail.com>
2026-07-18 09:51:47 +0530

    Typing

---
+++ TypingTest/script.js
@@ -0,0 +1,5 @@
+// Random Quotes Api URl
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const quoteSection = document.getElementById("quote");
+const userInput = document.getElementById("quote-input");
+console.log(quoteSection, userInput);
