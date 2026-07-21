dec91395d05416ed8a2e7e0df0a13516c74ed3bf
Rishab Raj <rr0948555@gmail.com>
2026-07-21 09:37:38 +0530

    Typing Test

--- TypingTest/script.js
+++ TypingTest/script.js
@@ -111,8 +111,9 @@ const displayResult = () => {
      if (time != 0) {
          timeTaken = (60 - time) / 100;
      }
-     document.getElementById("wpm").innerText = (userInput.value.length / 5 / timeTaken);
-}
+     document.getElementById("wpm").innerText = (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
+     document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100 ) + "%";
+};
 
 window.onload = () => {
     userInput.value = "";
