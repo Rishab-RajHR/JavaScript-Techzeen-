7c40fa6196082b1526f673d6d317ad63b9689734
Rishab Raj <rr0948555@gmail.com>
2026-07-23 09:44:11 +0530

    Password Strength

--- PasswordStrength/script.js
+++ PasswordStrength/script.js
@@ -8,7 +8,13 @@ let parameters = {
 let strengthBar = document.getElementById("strength-bar");
 let msg = document.getElementById("msg");
 
+function strengthChecker(){
+      let password = document.getElementById("password").ariaValueMax;
 
+      parameters.letters = (/[A-Za-z]+/.test(password))?true:false;
+      parameters.numbers = (/[0-9]+/.test(password))?true:false;
+      parameters.special = (/[!\"$%&/()=?@-`\\.\`;:+=^*_-]+/.test(password))?true:false;
+}
 
 function toggle() {
     let password = document.getElementById("password");
