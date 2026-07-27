9f4b59d95cdd323a16cfbee7306057caf2ee0512
Rishab Raj <rr0948555@gmail.com>
2026-07-27 09:38:19 +0530

    Aspect Ratio

--- AspectRatio/script.js
+++ AspectRatio/script.js
@@ -6,6 +6,14 @@ let height = document.getElementById("height");
 let calculateWidth = () => {
     let aspectRatio = ratioWidth.value / ratioHeight.value;
     width.value = parseFloat((height.value * aspectRatio).toFixed(2));
-}
+};
+
+let calculateHeight = () => {
+   let aspectRatio = ratioWidth.value / ratioHeight.value;
+   height.value = parseFloat((width.value / aspectRatio).toFixed(2));
+};
 
 height.addEventListener("input", calculateWidth);
+width.addEventListener("input", calculateHeight);
+ratioHeight.addEventListener("input", calculateHeight);
+ratioWidth.addEventListener("input", calculateHeight)
