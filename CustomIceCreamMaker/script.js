0a4e1d11e6a5e0856de270a4071f54af8a797cee
Rishab Raj <rr0948555@gmail.com>
2026-07-29 09:38:30 +0530

    Ice Cream Customized

---
+++ CustomIceCreamMaker/script.js
@@ -0,0 +1,11 @@
+let scoop1 = document.querySelector(".scoop1");
+let scoop2 = document.querySelector(".scoop2");
+let scoop3 = document.querySelector(".scoop3");
+
+let btnScoop1 =  document.getElementById("btn-scoop1");
+let btnScoop2 =  document.getElementById("btn-scoop2");
+let btnScoop3 =  document.getElementById("btn-scoop3");
+let btnTopping1 = document.getElementById("btn-topping1");
+let btnTopping2 = document.getElementById("btn-topping2");
+
+let colors = ["#f45c96", "#f8bd3d", "#badc58", "#5c0003", "#f58619", "#ebab70"];
