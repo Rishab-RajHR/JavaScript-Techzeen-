db53dda87533b34f699da9bbc6669667c944a28a
Rishab Raj <rr0948555@gmail.com>
2026-07-30 09:21:44 +0530

    Custom Ice Cream

--- CustomIceCreamMaker/script.js
+++ CustomIceCreamMaker/script.js
@@ -9,3 +9,14 @@ let btnTopping1 = document.getElementById("btn-topping1");
 let btnTopping2 = document.getElementById("btn-topping2");
 
 let colors = ["#f45c96", "#f8bd3d", "#badc58", "#5c0003", "#f58619", "#ebab70"];
+
+let counter1 = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
+let counter5 = 0;
+
+
+btnScoop1.addEventListener("click", () => {
+    scoop1.style.backgroundColor = colors[2];
+});
