25022c14babfbb53716cb826a5da38e9506c5f49
Rishab Raj <rr0948555@gmail.com>
2026-07-30 09:44:38 +0530

    Custom Ice Cream

--- CustomIceCreamMaker/script.js
+++ CustomIceCreamMaker/script.js
@@ -37,6 +37,11 @@ btnScoop3.addEventListener("click", () => {
 });
 
 btnTopping1.addEventListener("click", () => {
-    scoop1.style.backgroundColor = `radial-gradient(circle at 20px 18px, ${colors[counter4]} 25px, transparent 25px)`;
+    scoop1.style.backgroundImage = `radial-gradient(circle at 20px 18px, ${colors[counter4]} 25px, transparent 25px)`;
     counter4 = setCounterValue(counter4);
+});
+
+btnTopping2.addEventListener("click", () => {
+    scoop3.style.backgroundImage = `radial-gradient (circle ar 12.5px 17px, ${colors[counter5]} 20px, transparent 21px)`;
+    counter5 = setCounterValue(counter5);
 });
