ab34dff68977960db5a382a0ae7836eb7c2c37cc
Rishab Raj <rr0948555@gmail.com>
2026-08-01 09:52:13 +0530

    Monster maker

---
+++ MonsterMaker/script.js
@@ -0,0 +1,4 @@
+let colorBtn = document.getElementById("color-btn");
+let faceBtn = document.getElementById("face-btn");
+let hornsBtn = document.getElementById("horns-btn");
+let tailBtn = document.getElementById("tail-btn");
