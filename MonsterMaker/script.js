0206d515680809f0b326d708d917595cc6cc1fe6
Rishab Raj <rr0948555@gmail.com>
2026-08-02 09:33:54 +0530

    Monster Maker

--- MonsterMaker/script.js
+++ MonsterMaker/script.js
@@ -2,3 +2,14 @@ let colorBtn = document.getElementById("color-btn");
 let faceBtn = document.getElementById("face-btn");
 let hornsBtn = document.getElementById("horns-btn");
 let tailBtn = document.getElementById("tail-btn");
+
+let docRoot = document.querySelector(":root");
+
+let bodyColors = [
+    "#a8d530",
+    "#42aaff",
+    "#f3d5db",
+    "#ff4f51",
+    "#904ae8",
+    "#ffa711"
+];
