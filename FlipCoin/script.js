6ddfb021dcd8d2d535291bbf707d3d8d1b74fe6d
Rishab Raj <rr0948555@gmail.com>
2026-08-05 09:39:22 +0530

    Flip Coin

--- FlipCoin/script.js
+++ FlipCoin/script.js
@@ -19,4 +19,12 @@ flipBtn.addEventListener("click", () => {
        }, 100);
        tails++;
     }
+    setTimeout(updateStats, 3000);
+
 });
+function updateStats(){
+    document.querySelector("#heads-count");
+    textContent = `Heads; ${heads}`;
+    document.querySelector("#tails-count");
+    textContent = `Tails: ${tails}`;
+}
