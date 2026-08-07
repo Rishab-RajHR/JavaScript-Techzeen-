8b54cbf5c7612a9ce9838744fbe9d6dae9df5096
Rishab Raj <rr0948555@gmail.com>
2026-08-07 09:32:16 +0530

    Random Hex

---
+++ RandomHexCodeGenerator/script.js
@@ -0,0 +1,4 @@
+let outputColor = document.querySelector("#output-color span");
+let output = document.getElementById("output");
+let genBtn = document.getElementById("gen-btn");
+let copyBtn = document.getElementById("copy-btn"); 
