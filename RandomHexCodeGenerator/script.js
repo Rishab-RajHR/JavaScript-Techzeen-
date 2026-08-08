b7ab2384efe74554f3d3308dfe5632b3da7535bc
Rishab Raj <rr0948555@gmail.com>
2026-08-08 09:43:17 +0530

    Random hex

--- RandomHexCodeGenerator/script.js
+++ RandomHexCodeGenerator/script.js
@@ -19,5 +19,14 @@ let genHexCode = () => {
     outputColor.style.backgroundColor = hexCode;
 }
 
+copyBtn.addEventListener("click", () => {
+     output.select();
+     document.execCommand("copy");
+     customAlert.style.transform = "translateX(0)";
+     setTimeout(() => {
+          customAlert.style.transform = "translateX(calc( 100% + 10px ))";
+     }, 2000);
+})
+
 window.onload = genHexCode;
 genBtn.addEventListener("click", genHexCode);
