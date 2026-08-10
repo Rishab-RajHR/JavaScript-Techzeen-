b8aa2ba45a914cace6c30115266c4cf9916981d6
Rishab Raj <rr0948555@gmail.com>
2026-08-10 09:34:14 +0530

    Box-Shadow

--- BoxShadow/script.js
+++ BoxShadow/script.js
@@ -5,5 +5,9 @@ let inputs = document.querySelectorAll(".sliders input");
 inputs.forEach((inp) => inp.addEventListener("input", generateShadow));
 
 function generateShadow(){
-    
+     let hShadow = document.getElementById("h-shadow").value;
+     let vShadow = document.getElementById("v-shadow").value;
+     let blurRadius = document.getElementById("blur-radius").value;
+     let spreadRadius = document.getElementById("spread-radius").value;
+
 }
