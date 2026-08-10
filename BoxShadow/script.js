97c52da201335ebd85b2544a8acb10ec9a8deebd
Rishab Raj <rr0948555@gmail.com>
2026-08-10 09:29:35 +0530

    Box Shadow

---
+++ BoxShadow/script.js
@@ -0,0 +1,9 @@
+let elem = document.getElementById("element");
+let code = document.getElementById("code");
+let inputs = document.querySelectorAll(".sliders input");
+
+inputs.forEach((inp) => inp.addEventListener("input", generateShadow));
+
+function generateShadow(){
+    
+}
