0c7834d6acff9a7177401c0a7829a807fe0fbdff
Rishab Raj <rr0948555@gmail.com>
2026-08-14 09:38:56 +0530

    Predictive Text

--- PredictiveText/script.js
+++ PredictiveText/script.js
@@ -43,6 +43,12 @@ const caseCheck = (word) => {
      // loop through every character in ino
      for(let i in inp){
         // if input character matches with character in word no need to change
+        if(inp[i] == word[i]) {
+           continue;
+        } else if (inp[i].toUpperCase() == word[i]) {
+           // If inp[i] when converted to uppercase matches word[i] it means word[i] needs to be lowercase
+           word.splice(i, 1, word[i].toLowerCase());
+        }
      }
 }
 
