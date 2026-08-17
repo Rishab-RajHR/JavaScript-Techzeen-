0917ec54218fc166e4d8b505ef143eb56ab916db
Rishab Raj <rr0948555@gmail.com>
2026-08-17 09:38:43 +0530

    Pokemon Card

--- PokemonCard/script.js
+++ PokemonCard/script.js
@@ -17,3 +17,13 @@
    water: "#0190FF"
 
 */
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById("card");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", getPokeData);
+
+let getPokeData = () => {
+    // Generate a random number between 1 and 150
+}
