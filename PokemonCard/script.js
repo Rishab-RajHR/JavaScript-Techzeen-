582ac1b385ea41e8dd3dce12c0ce2fa36fd8a468
Rishab Raj <rr0948555@gmail.com>
2026-08-18 09:26:27 +0530

    Pokemon Card

--- PokemonCard/script.js
+++ PokemonCard/script.js
@@ -45,6 +45,9 @@ let generateCard = (data) => {
      const hp = data.stats[0].base_stat;
      const imgSrc = data.sprites.other.dream_world.front_default;
      const pokeName = data.name;
+     const statAttack = data.stats[1].base_stat;
+     const statDefense = data.stats[2].base_stat;
+     const statSpeed = data.stats[5].base_stat;
 }
 
 btn.addEventListener("click", getPokeData);
