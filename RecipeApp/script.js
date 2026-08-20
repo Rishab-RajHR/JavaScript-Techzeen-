f0d98c0dc744a2bf7f654d0948d15076aa278098
Rishab Raj <rr0948555@gmail.com>
2026-08-20 09:49:34 +0530

    Recipe App

--- RecipeApp/script.js
+++ RecipeApp/script.js
@@ -1,1 +1,12 @@
-// Initial Reference
+// Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?=";
+
+let userInp = document.getElementById("user-inp").value;
+
+fetch(url + "big mac")
+   .then((response) => response.json())
+   .then((data) => {
+       console.log(data);
+   });
