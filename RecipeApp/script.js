c189433490836744296c798a7964a5d28250c056
Rishab Raj <rr0948555@gmail.com>
2026-08-23 09:53:18 +0530

    Recipe App

--- RecipeApp/script.js
+++ RecipeApp/script.js
@@ -3,7 +3,9 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?=";
 
-let userInp = document.getElementById("user-inp").value;
+searchBtn.addEventListener("click",() => {
+     let userInp = document.getElementById("user-inp").value;
+})
 
 fetch(url + "big mac")
    .then((response) => response.json())
@@ -56,8 +58,8 @@ fetch(url + "big mac")
 
         hideRecipe.addEventListener("click", () => {
             recipe.style.display = "none";
-        })
+        });
         showRecipe.addEventListener("click", () => {
             recipe.style.display = "block";
-        })
+        });
    });
