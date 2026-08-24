e3d3c05259bc50d59903690947d77fabb85ae44e
Rishab Raj <rr0948555@gmail.com>
2026-08-24 09:50:30 +0530

    Auto Complete

--- autoComplete/script.js
+++ autoComplete/script.js
@@ -4,11 +4,12 @@
 let names = [
    "DevPulse","CloudForge","SmartStack","LogicLoop","CodeSprint","WebMatrix","DigitalNest","FutureCode","AlphaTech","NovaStack","TechFusion","ByteWorks","CodeWave","DevOrbit","StackNova","PixelStack","QuantumCode"
 ];
-let sortedNames = names.sort();
+// Sort names in ascending order
 console.log(sortedNames);
 
 
-
+// Reference 
+let input = document.getElementById("input");
 
 
 
