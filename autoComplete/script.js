a9f22412c90f8a263fc827d29ee3f0c393107d1e
Rishab Raj <rr0948555@gmail.com>
2026-08-25 09:48:53 +0530

    Auto Complete

--- autoComplete/script.js
+++ autoComplete/script.js
@@ -25,6 +25,8 @@ input.addEventListener("keyup", (e) => {
           listItem.classList.add("list-items");
           listItem.style.cursor = "pointer";
           listItem.setAttribute("onclick", "displayNames('" + i + "')");
+          // Display matched part in bold
+          let word = "<b>" + i.substr(0,input.value.length) + "</b>"
        }
      }
 });
