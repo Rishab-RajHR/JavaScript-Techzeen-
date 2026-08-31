820696d2409973b9d1a059478a4d6e5c5dc8d3bf
Rishab Raj <rr0948555@gmail.com>
2026-08-31 09:18:22 +0530

    Captcha

--- Captcha/script.js
+++ Captcha/script.js
@@ -1,3 +1,5 @@
 // Initial References
 let submitButton = document.getElementById("submit-button");
 let userInput = document.getElementById("user-input");
+let textOutput = document.getElementById("text");
+let canvas = document.getElementById("canvas");
