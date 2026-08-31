cadc7fa8fe61cf94f0fb3de4d05107277d422feb
Rishab Raj <rr0948555@gmail.com>
2026-08-31 09:29:31 +0530

    Captcha

--- Captcha/script.js
+++ Captcha/script.js
@@ -10,7 +10,16 @@ const textGenerator = () => {
    let generatedText = "";
    /* String.fromCharCode gives ASCII value from a given number */
    // Total 9 letters hence loop of 3
-}
+   for(let i=0; i<3; i++) {
+     // 65-90 numbers are capital letters
+     generatedText += String.fromCharCode(randomNumber(65, 90));
+     // 97-122 are small letters
+      generatedText += String.fromCharCode(randomNumber(97, 122));
+      // 48-57 are numbers from 0-9
+      generatedText += String.fromCharCode(randomNumber(48, 57));
+   }
+   return generatedText;
+};
 
 // Generate random Numbers between a given range
 const randomNumber = (min, max) => 
