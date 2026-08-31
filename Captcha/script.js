03abdf7b9e5b4f122339c8908180e7388f9161a4
Rishab Raj <rr0948555@gmail.com>
2026-08-31 09:21:54 +0530

    Captcha

--- Captcha/script.js
+++ Captcha/script.js
@@ -4,3 +4,9 @@ let userInput = document.getElementById("user-input");
 let canvas = document.getElementById("canvas");
 let reloadButton = document.getElementById("reload-button");
 let text = "";
+
+// Generate random Numbers between a given range
+const randomNumber = (min, max) => 
+      Math.floor(Math.random() * (max - min + 1) + min);
+
+console.log(randomNumber(1, 10));
