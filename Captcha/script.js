dc467986f74523436788034336d5f05b2020d6e4
Rishab Raj <rr0948555@gmail.com>
2026-08-31 09:26:45 +0530

    Captcha

--- Captcha/script.js
+++ Captcha/script.js
@@ -5,8 +5,13 @@ let canvas = document.getElementById("canvas");
 let reloadButton = document.getElementById("reload-button");
 let text = "";
 
+// Generate Text
+const textGenerator = () => {
+   let generatedText = "";
+   /* String.fromCharCode gives ASCII value from a given number */
+   // Total 9 letters hence loop of 3
+}
+
 // Generate random Numbers between a given range
 const randomNumber = (min, max) => 
       Math.floor(Math.random() * (max - min + 1) + min);
-
-console.log(randomNumber(1, 10));
