6417fb1c6ee026978f2c084a787e201326d0a99d
Rishab Raj <rr0948555@gmail.com>
2026-09-05 09:35:55 +0530

    Responsive Number

--- ResponsiveNumber/script.js
+++ ResponsiveNumber/script.js
@@ -5,5 +5,11 @@ valueDisplays.forEach((valueDisplays) => {
     let startValue = 0;
     let endValue = parseInt(valueDisplays.getAttribute("data-val"));
     let duration = Math.floor(interval / endValue);
-    
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplays.textContent = startValue;
+        if(startValue === endValue) {
+           clearInterval(counter);
+        }
+    })
 });
