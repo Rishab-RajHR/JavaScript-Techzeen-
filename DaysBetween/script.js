863a83d56661723385e7152ccee7cbb046101bee
Rishab Raj <rr0948555@gmail.com>
2026-09-07 09:46:11 +0530

    Days Between

--- DaysBetween/script.js
+++ DaysBetween/script.js
@@ -1,3 +1,9 @@
 // Declaring and initializing variables
 let submit = document.getElementById("submit");
 let output = document.getElementById("output");
+
+submit.addEventListener("click", () => {
+    // Create a Date Object from input value
+    let date1 = new Date(document.getElementById("date-1").value);
+    let date2 = new Date(document.getElementById("date-2").value);
+})
