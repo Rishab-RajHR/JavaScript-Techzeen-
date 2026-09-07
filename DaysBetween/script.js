3e0f99ddc2511a85e0f5773113c320b0d5de1f76
Rishab Raj <rr0948555@gmail.com>
2026-09-07 09:46:36 +0530

    Days Between

--- DaysBetween/script.js
+++ DaysBetween/script.js
@@ -6,4 +6,5 @@ submit.addEventListener("click", () => {
     // Create a Date Object from input value
     let date1 = new Date(document.getElementById("date-1").value);
     let date2 = new Date(document.getElementById("date-2").value);
+    console.log(date1, date2);
 })
