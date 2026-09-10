4681d2c3f26dbbe58eee9a45d3637b309e8aa13f
Rishab Raj <rr0948555@gmail.com>
2026-09-10 09:42:48 +0530

    Currency Formatter

--- CurrencyFormat/script.js
+++ CurrencyFormat/script.js
@@ -6,3 +6,10 @@ let formatter1 = new Intl.NumberFormat(
     "en-IN",
     {style: "currency", currency: "INR"}
 );
+
+btn.addEventListener("click", ()=>{
+    let amount = document.getElementById("amount").nodeValue;
+
+    rupees.innerHTML = `<span>Indian Rupees:</span>${formatter1.format(amount)}`;
+
+});
